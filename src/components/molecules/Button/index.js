cf1b85b22a5c94e47ed876fe9a4db3497e22426d
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-06 15:59:43 +0200

    feat(Button): make button clickable

--- src/components/molecules/Button/index.js
+++ src/components/molecules/Button/index.js
@@ -2,11 +2,15 @@ import { html, LitElement } from '@polymer/lit-element';
 import CSS from './_components.button-css';
 
 class Button extends LitElement {
+  handleClick () {
+    console.log('button click');
+  }
+
   _render () {
     return html`
-    ${CSS}
-    <button class='buv-c-button'>Verify</button>
-  `;
+      ${CSS}
+      <button class='buv-c-button' on-click='${this.handleClick}'>Verify</button>
+    `;
   }
 }
 
