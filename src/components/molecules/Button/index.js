6ef11f7b0ca5621d391668c077e4abfeaf75f57d
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-06 16:29:49 +0200

    feat(Button): handle internal property to modify content

--- src/components/molecules/Button/index.js
+++ src/components/molecules/Button/index.js
@@ -2,14 +2,28 @@ import { html, LitElement } from '@polymer/lit-element';
 import CSS from './_components.button-css';
 
 class Button extends LitElement {
+  constructor () {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    this.showSpinner = false;
+  }
+
+  static get properties() {
+    return {
+      showSpinner: Boolean
+    }
+  }
+
   handleClick () {
-    console.log('button click');
+    this.showSpinner = !this.showSpinner;
   }
 
   _render () {
     return html`
       ${CSS}
-      <button class='buv-c-button' on-click='${this.handleClick}'>Verify</button>
+      <button class='buv-c-button' on-click='${this.handleClick}'>
+       ${this.showSpinner ? 'Spinner' : 'Verify'}
+      </button>
     `;
   }
 }
