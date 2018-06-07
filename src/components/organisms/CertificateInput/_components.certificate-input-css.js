9ab0dbae6cb658b6c1e412b4e5fd40e660fbb022
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-07 16:32:17 +0200

    feat(Input): fix styling

--- src/components/organisms/CertificateInput/_components.certificate-input-css.js
+++ src/components/organisms/CertificateInput/_components.certificate-input-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-c-certificate-input{display:flex;width:100%}
+export default html`<style>.buv-c-certificate-input{display:flex;width:100%}.buv-c-certificate-input__input{width:100%}
 </style>`;
--- src/components/organisms/CertificateInput/index.js
+++ src/components/organisms/CertificateInput/index.js
@@ -7,7 +7,7 @@ import CSS from './_components.certificate-input-css';
 const CertificateInput = html`
     ${CSS}
     <div class="buv-c-certificate-input">
-        <buv-input></buv-input><buv-button></buv-button>        
+        <buv-input class="buv-c-certificate-input__input"></buv-input><buv-button></buv-button>        
     </div>
 `;
 
