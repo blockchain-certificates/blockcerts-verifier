ab43852bb45c9b1bbaeec97ceb748f32ad94beaf
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-07 12:02:52 +0200

    feat(CertificateInput): styling

---
+++ src/components/organisms/CertificateInput/_components.certificate-input-css.js
@@ -0,0 +1,3 @@
+import {html} from '@polymer/lit-element';
+export default html`<style>.buv-c-certificate-input{display:flex;width:100%}
+</style>`;
--- src/components/organisms/CertificateInput/index.js
+++ src/components/organisms/CertificateInput/index.js
@@ -2,8 +2,13 @@ import { html } from '@polymer/lit-element';
 import Input from '../../atoms/Input';
 import '../../molecules/Button';
 
+import CSS from './_components.certificate-input-css';
+
 const CertificateInput = html`
-  ${Input}<buv-button></buv-button>
+    ${CSS}
+    <div class="buv-c-certificate-input">
+        ${Input}<buv-button></buv-button>        
+    </div>
 `;
 
 export default CertificateInput;
