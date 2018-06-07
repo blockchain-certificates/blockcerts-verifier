f093593adb5898b1b0fcc4335fbdf770b2e33b39
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-07 14:54:49 +0200

    feat(Store): add updateCertificateUrl action creator

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,11 @@
+import * as ACTIONS from '../constants/actionTypes';
+import updateCertificateUrl from './updateCertificateUrl';
+
 export function app (state, action) {
   switch (action.type) {
+    case ACTIONS.UPDATE_CERTIFICATE_URL:
+      return updateCertificateUrl(state, action);
+
     default:
       return state
   }
