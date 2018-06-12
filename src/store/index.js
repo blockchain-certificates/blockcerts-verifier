ad7317d354dcd8f1f2efc386e50d87f5c314cdf3
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-12 10:25:47 +0200

    feat(Store): implement own version of redux-logger to log redux lifecycle

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
+import { logger } from './logger/src/';
 import { app } from '../reducers';
+import { ENV } from '../utils/environment';
 
 export function configureStore ({ initialState = {} } = {}) {
   const middlewares = [];
+  if (ENV.debugEnabled) {
+    // TODO: ensure the logger code does not make it to production build
+    middlewares.push(logger);
+  }
 
   return createStore(
     app,
