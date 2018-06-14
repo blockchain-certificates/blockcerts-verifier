94954b6d53e72c8e39e9259b846ed143f4286c91
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-14 18:44:13 +0200

    chore(MockServer): remove unused dependency

--- test/mock-server/index.js
+++ test/mock-server/index.js
@@ -1,8 +1,7 @@
 import mockCertificate from '../fixtures/certificate-example';
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
 
 app.get('/to/certificate', (req, res) => res.send(mockCertificate));
 
-server.listen(3001, () => console.log('mock server started on port 3001!'));
+app.listen(3001, () => console.log('mock server started on port 3001!'));
