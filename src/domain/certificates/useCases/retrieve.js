6bcd82b071e237c360e4000a73c5c432f20df763
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-14 16:23:43 +0200

    test(Certificate): do nothing if url is invalid

--- src/domain/certificates/useCases/retrieve.js
+++ src/domain/certificates/useCases/retrieve.js
@@ -1,4 +1,15 @@
+function isValidUrl (url) {
+  // https://stackoverflow.com/a/15734347/4064775
+  const regex = /^(ftp|http|https):\/\/[^ "]+$/;
+  return regex.test(url);
+}
+
 export default function retrieve (url) {
+  if (!isValidUrl(url)) {
+    console.error('Invalid url to retrieve:', url);
+    return null;
+  }
+
   const param = '?format=json';
   const urlWithParam = url + param;
 
--- test/application/domain/certificates/useCases/retrieve.test.js
+++ test/application/domain/certificates/useCases/retrieve.test.js
@@ -2,15 +2,22 @@ import domain from '../../../../../src/domain';
 import certificateFixture from '../../../../fixtures/certificate-example';
 
 describe('domain certificates retrieve method test suite', function () {
-  describe('given a url', function () {
+  describe('given a valid url', function () {
     it('fetches the json format from that url', async function () {
       const url = 'http://localhost:3001/to/certificate';
       const result = await domain.certificates.retrieve(url);
 
       expect(result).toEqual(certificateFixture);
     });
+    // TODO: handle failing case (no JSON answer)
   });
 
-  // TODO: handle failing case (no JSON answer)
-  // TODO: add safe guards against invalid URL
+  describe('given an invalid url', function () {
+    it('should return null', function () {
+      const url = 'this is not a url';
+
+      const result = domain.certificates.retrieve(url);
+      expect(result).toBe(null);
+    })
+  });
 });
