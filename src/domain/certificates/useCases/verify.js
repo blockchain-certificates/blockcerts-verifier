88ef0bfa1caac14fb13d9305493c46b4c485c09f
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-22 22:45:53 +0200

    feat(Verifier): domain calls verifier

--- src/domain/certificates/useCases/verify.js
+++ src/domain/certificates/useCases/verify.js
@@ -1,7 +1,17 @@
-import { CertificateVerifier } from 'cert-verifier-js/lib';
+import { CertificateVerifier } from 'cert-verifier-js';
 
-export default function verify (certificateDefinition) {
-  console.log(certificateDefinition);
-  const verifier = new CertificateVerifier();
-  console.log(verifier);
+export default async function verify (certificateDefinition) {
+  const certificateAsString = JSON.stringify(certificateDefinition);
+  const verifier = new CertificateVerifier(certificateAsString, (step, status, message) => {
+    console.log('stepCode', step);
+    console.log('status', status);
+    console.log('message', message);
+  });
+  const res = await verifier.verify((stepCode, status, message) => {
+    console.log('** done **');
+    console.log('stepCode', stepCode);
+    console.log('status', status);
+    console.log('message', message);
+  });
+  console.log(res);
 }
