d5bc79fbe844319a5c6e69afeb53b69f4853f1f9
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-27 20:55:04 +0200

    test(StepCreate): test output not implementation

--- test/application/domain/verification/useCases/create.spec.js
+++ test/application/domain/verification/useCases/create.spec.js
@@ -1,11 +1,49 @@
 import domain from '../../../../../src/domain';
-import { Step } from '../../../../../src/domain/verification/entities';
 
 describe('domain verification create use case test suite', function () {
   describe('given it is called with a step definition', function () {
-    it('should return an instance of a step entity', function () {
-      const name = 'Jean Michel';
-      const code = 'jeanmimi';
+    const name = 'Jean Michel';
+    const code = 'jeanmimi';
+    let definition;
+    let sut;
+
+    beforeEach(function () {
+      definition = {
+        name,
+        code
+      };
+
+      sut = domain.verification.createStep(definition);
+    });
+
+    afterEach(function () {
+      definition = null;
+      sut = null;
+    });
+
+    it('should should set the code of the step', function () {
+      expect(sut.name).toBe(name);
+    });
+
+    it('should should set the default status of the step', function () {
+      expect(sut.code).toBe(code);
+    });
+
+    it('should should set the substeps property of the step', function () {
+      expect(sut.substeps).toEqual([]);
+    });
+
+    describe('given the step does not have a parent', function () {
+      it('should not set the parentStep property', function () {
+        expect(sut.parentStep).toBe(undefined);
+      });
+    });
+  });
+
+  describe('given the step has a parent', function () {
+    it('should set the parentStep property', function () {
+      const name = 'Computing local hash';
+      const code = 'computingLocalHash';
       const definition = {
         name,
         code
@@ -13,7 +51,7 @@ describe('domain verification create use case test suite', function () {
 
       const sut = domain.verification.createStep(definition);
 
-      expect(sut instanceof Step).toBe(true);
+      expect(sut.parentStep).toBe('formatValidation');
     });
   });
 });
