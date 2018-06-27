1eb3de4699377269aa713bcd7424fe77cce3d167
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-27 17:49:54 +0200

    feat(Verification): fix display of steps and substeps

--- src/components/atoms/VerificationStep/VerificationStep.js
+++ src/components/atoms/VerificationStep/VerificationStep.js
@@ -1,5 +1,11 @@
 import { html } from '@polymer/lit-element';
 
-export default function VerificationStep ({ stepName, status }) {
-  return html`<li>${stepName}: ${status}</li>`;
+export default function VerificationStep ({ name, status }, parent) {
+  let innerHTML
+  if (parent) {
+    innerHTML = html`<dt>${name}: ${status}</dt>`
+  } else {
+    innerHTML = html`<dd>${name}: ${status}</dd>`
+  }
+  return html`${innerHTML}`;
 }
--- src/components/organisms/VerificationProcess/VerificationProcess.js
+++ src/components/organisms/VerificationProcess/VerificationProcess.js
@@ -3,12 +3,13 @@ import VerificationStep from '../../atoms/VerificationStep';
 
 export default function VerificationProcess ({ steps }) {
   const innerHTML = steps.map(step => html`
-    ${VerificationStep(step)}
+    ${VerificationStep(step, parent)}
+      ${step.substeps.map(substep => html`${VerificationStep(substep)}`)}
   `);
 
   return html`
-    <ul>
+    <dl>
         ${innerHTML}
-    </ul>
+    </dl>
   `;
 }
