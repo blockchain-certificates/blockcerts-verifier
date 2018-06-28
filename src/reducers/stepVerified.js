18480de39559e89e920b279973dafc8d77aa6c46
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-28 14:25:58 +0200

    feat(Verification): deduplicate similar steps display

--- src/reducers/stepVerified.js
+++ src/reducers/stepVerified.js
@@ -1,10 +1,22 @@
 import { getVerifiedSteps, getParentStep } from '../selectors/certificate';
 
+function saveSubstepIn (parent, substep) {
+  // does substep exist in parent yet?
+  const substepIndex = parent.substeps.findIndex(s => s.code === substep.code);
+
+  if (substepIndex > -1) {
+    parent.substeps[substepIndex] = substep;
+  } else {
+    parent.substeps.push(substep);
+  }
+}
+
 export default function stepVerified (state, action) {
   const { parentStep, code, name, status } = action.payload;
   const storedParentState = getParentStep(state, parentStep);
+
   if (parentStep && storedParentState) {
-    storedParentState.substeps.push({ code, name, status });
+    saveSubstepIn(storedParentState, { code, name, status });
   } else {
     getVerifiedSteps(state).push(action.payload);
   }
--- test/assertions/validCertificateSteps.js
+++ test/assertions/validCertificateSteps.js
@@ -4,13 +4,9 @@ export default [
     name: 'Format validation',
     status: 'standby',
     substeps: [
-      {'status': 'starting', 'code': 'getTransactionId', 'name': 'Getting transaction ID'},
       {'status': 'success', 'code': 'getTransactionId', 'name': 'Getting transaction ID'},
-      {'status': 'starting', 'code': 'computingLocalHash', 'name': 'Computing Local Hash'},
       {'status': 'success', 'code': 'computingLocalHash', 'name': 'Computing Local Hash'},
-      {'status': 'starting', 'code': 'fetchingRemoteHash', 'name': 'Fetching remote hash'},
       {'status': 'success', 'code': 'fetchingRemoteHash', 'name': 'Fetching remote hash'},
-      {'status': 'starting', 'code': 'parsingIssuerKeys', 'name': 'Parsing issuer keys'},
       {'status': 'success', 'code': 'parsingIssuerKeys', 'name': 'Parsing issuer keys'}
     ]
   },
@@ -19,11 +15,8 @@ export default [
     name: 'Hash comparison',
     status: 'standby',
     substeps: [
-      {'status': 'starting', 'code': 'comparingHashes', 'name': 'Comparing Hashes'},
       {'status': 'success', 'code': 'comparingHashes', 'name': 'Comparing Hashes'},
-      {'status': 'starting', 'code': 'checkingMerkleRoot', 'name': 'Checking Merkle Root'},
       {'status': 'success', 'code': 'checkingMerkleRoot', 'name': 'Checking Merkle Root'},
-      {'status': 'starting', 'code': 'checkingReceipt', 'name': 'Checking Receipt'},
       {'status': 'success', 'code': 'checkingReceipt', 'name': 'Checking Receipt'}
     ]
   },
@@ -32,11 +25,8 @@ export default [
     name: 'Status check',
     status: 'standby',
     substeps: [
-      {'status': 'starting', 'code': 'checkingRevokedStatus', 'name': 'Checking Revoked Status'},
       {'status': 'success', 'code': 'checkingRevokedStatus', 'name': 'Checking Revoked Status'},
-      {'status': 'starting', 'code': 'checkingAuthenticity', 'name': 'Checking Authenticity'},
       {'status': 'success', 'code': 'checkingAuthenticity', 'name': 'Checking Authenticity'},
-      {'status': 'starting', 'code': 'checkingExpiresDate', 'name': 'Checking Expires Date'},
       {'status': 'success', 'code': 'checkingExpiresDate', 'name': 'Checking Expires Date'}
     ]
   },
