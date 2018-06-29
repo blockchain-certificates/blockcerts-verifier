468043216a2278276e795b756f97b24b10ec908b
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-29 14:45:22 +0200

    feat(Verification): progress bar in error

--- src/components/organisms/VerificationProcess/VerificationProcess.js
+++ src/components/organisms/VerificationProcess/VerificationProcess.js
@@ -1,12 +1,19 @@
 import { html } from '@polymer/lit-element';
 import VerificationStep from '../../molecules/VerificationStep';
 import CSS from './_components.verification-process-css';
+import * as VERIFICATION_STATUS from '../../../constants/verificationStatus';
 
 export default function VerificationProcess ({ steps }) {
   const innerHTML = steps.map(step => html`
     ${VerificationStep(step, true)}
     ${step.substeps.map(substep => html`${VerificationStep(substep)}`)}
   `);
+
+  // TODO: this should likely not be determined in the view
+  const hasError = steps.some(s => s.status === VERIFICATION_STATUS.FAILURE);
+
+  // TODO: better handle this dynamic class (cf npm classnames)
+  const progressBarClasses = `buv-c-verification-progress-bar ${hasError ? 'has-errored' : ''}`;
 
   if (!innerHTML.length) {
     return;
@@ -15,7 +22,7 @@ export default function VerificationProcess ({ steps }) {
   return html`
     ${CSS}
     <div class='buv-c-verification-process'>
-      <div class='buv-c-verification-progress-bar'></div>  
+      <div class$='${progressBarClasses}'></div>  
       <dl class='buv-c-verification-process__step-list'>
         ${innerHTML}
         <dd></dd>
