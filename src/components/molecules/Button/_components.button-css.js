253a188656368b97b25d12cb031ae099e89309e7
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-03 16:04:26 +0200

    feat(Verification): disallow verification until a certificate is present

--- src/components/molecules/Button/_components.button-css.js
+++ src/components/molecules/Button/_components.button-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-c-button{background-color:#1f845c;border-radius:0 2px 2px 0;color:#fff;cursor:pointer;display:block;font-size:15px;font-weight:500;padding:12px 38px;border:0 none;height:100%}
+export default html`<style>.buv-c-button{background-color:#1f845c;border-radius:0 2px 2px 0;color:#fff;cursor:pointer;display:block;font-size:15px;font-weight:500;padding:12px 38px;border:0 none;height:100%}.buv-c-button.is-disabled{cursor:default;filter:grayscale(100%)}
 </style>`;
