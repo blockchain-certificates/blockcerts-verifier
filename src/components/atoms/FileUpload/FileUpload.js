ddaa3be2d3f7823366fd75e7fa77e30db641ffcc
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-03 11:23:40 +0200

    feat(FileUpload): style component

--- src/components/atoms/FileUpload/FileUpload.js
+++ src/components/atoms/FileUpload/FileUpload.js
@@ -1,8 +1,10 @@
 import { html } from '@polymer/lit-element';
+import CSS from './_components.file-upload-css';
 
 export default function FileUpload () {
   return html`
-    <label for='buv-json-file-upload'>
+    ${CSS}
+    <label for='buv-json-file-upload' class='buv-c-file-upload'>
         Choose JSON file
         <input type='file' accept='application/json' id='buv-json-file-upload' class='buv-u-visually-hidden'/>
     </label>`;
