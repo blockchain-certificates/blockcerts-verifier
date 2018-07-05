763b529feec47e6b55cb91df1cef3351922fb111
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-05 12:15:10 +0200

    feat(DragAndDrop): add basic drag events

--- src/components/atoms/DragAndDrop/DragAndDrop.js
+++ src/components/atoms/DragAndDrop/DragAndDrop.js
@@ -1,8 +1,40 @@
 import { html, LitElement } from '@polymer/lit-element';
 
 class DragAndDrop extends LitElement {
+  constructor () {
+    super();
+    this.isDraggedOver = false;
+
+    this.handleDragOver = this.handleDragOver.bind(this);
+    this.handleDragLeave = this.handleDragLeave.bind(this);
+    this.handleDrop = this.handleDrop.bind(this);
+  }
+
+  static get properties () {
+    return {
+      isDraggedOver: Boolean
+    };
+  }
+
+  handleDragOver () {
+    this.isDraggedOver = true;
+  }
+
+  handleDragLeave () {
+    this.isDraggedOver = false;
+  }
+
+  handleDrop () {
+    this.isDraggedOver = false;
+  }
+
   _render () {
-    return html`<div>
+    return html`
+    <div 
+      ondragover='${this.handleDragOver}'
+      ondragleave='${this.handleDragLeave}'
+      ondrop='${this.handleDrop}'
+    >
       <h1>Drag here</h1>
       <slot></slot>
     </div>`;
