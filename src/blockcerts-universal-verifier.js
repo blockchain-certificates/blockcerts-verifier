75ea91e5542e1d23eba265441dbb708840af4af6
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-05 16:40:08 +0200

    feat(API): component able to receive a src of a certificate

--- src/blockcerts-universal-verifier.js
+++ src/blockcerts-universal-verifier.js
@@ -5,7 +5,35 @@ import './components/organisms/VerificationProcess';
 import CSS from './components/atoms/GlobalStylesheet';
 
 class BlockcertsUniversalVerifier extends LitElement {
-  _render () {
+  static get properties () {
+    return {
+      src: String,
+      certificate: Object
+    }
+  }
+
+  _firstRendered () {
+    console.log('first rendered', this._props);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+      if(xmlhttp.status === 200 && xmlhttp.readyState === 4){
+        this.certificate = xmlhttp.responseText;
+      }
+    }.bind(this);
+    xmlhttp.open("GET", this.src, true);
+    xmlhttp.send();
+  }
+
+  _propertiesChanged (props, changedProps, prevProps) {
+    this._props = props;
+    console.log(this._props);
+
+    super._propertiesChanged(props, changedProps, prevProps)
+  }
+
+  _render (_props) {
+    console.log('render', this._props);
+
     return html`
       ${CSS}
       <section class='buv-c-verifier-main'>
