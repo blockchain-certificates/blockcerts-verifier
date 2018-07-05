313ce30ac0b94819f2d5d8803ee266fafc975c6d
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-05 19:23:37 +0200

    style(BUV): remove unused code

--- src/blockcerts-universal-verifier/BlockcertsUniversalVerifier.js
+++ src/blockcerts-universal-verifier/BlockcertsUniversalVerifier.js
@@ -19,11 +19,6 @@ class BlockcertsUniversalVerifier extends LitElement {
     }
   }
 
-  _propertiesChanged (props, changedProps, prevProps) {
-    this._props = props;
-    super._propertiesChanged(props, changedProps, prevProps);
-  }
-
   _render (_props) {
     return html`
       ${CSS}
@@ -44,8 +39,7 @@ window.customElements.define('buv-raw', BlockcertsUniversalVerifier);
 // wrap Button in order to plug into Container
 // necessary trade-off to deal with class component in the store connector
 function BUVWrapper (props) {
-  return html`
-  <buv-raw src='${props.src}' onLoad='${props.onLoad}'></buv-raw>`;
+  return html`<buv-raw src='${props.src}' onLoad='${props.onLoad}'></buv-raw>`;
 }
 
 export {
