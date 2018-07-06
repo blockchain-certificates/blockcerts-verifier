5c1e580bb178a33ae2bda5e212822dddb3ca4093
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-06 10:36:55 +0200

    feat(Validate): validate certificate definition

--- src/domain/certificates/useCases/index.js
+++ src/domain/certificates/useCases/index.js
@@ -1,9 +1,11 @@
 import retrieve from './retrieve';
 import read from './read';
+import validate from './validate';
 import verify from './verify';
 
 export {
   retrieve,
   read,
+  validate,
   verify
 };
---
+++ test/application/domain/certificates/useCases/validate.test.js
@@ -0,0 +1,30 @@
+import domain from '../../../../../src/domain';
+import certificateFixture from '../../../../fixtures/valid-certificate-example';
+
+const notACertificateDefinition = {
+  name: 'johnny',
+  age: '45',
+  occupation: 'punk rocker'
+};
+
+describe('domain certificates validate method test suite', function () {
+  describe('given a valid definition of a certificate', function () {
+    it('should return an object with isValid property set to true', function () {
+      expect(domain.certificates.validate(certificateFixture).isValid).toBe(true);
+    });
+
+    it('should return an object with errorMessage property undefined', function () {
+      expect(domain.certificates.validate(certificateFixture).errorMessage).toBe(undefined);
+    });
+  });
+
+  describe('given an invalid definition of a certificate', function () {
+    it('should return an object with isValid property set to false', function () {
+      expect(domain.certificates.validate(notACertificateDefinition).isValid).toBe(false);
+    });
+
+    it('should return an object with errorMessage property describing the error', function () {
+      expect(domain.certificates.validate(notACertificateDefinition).errorMessage).toBe('Not a valid Blockcerts certificate.');
+    });
+  });
+});
