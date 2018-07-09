93ae9e89472cb746a45d5a6f904b7533f2e50984
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-09 15:16:38 +0200

    feat(Card): styles

--- src/components/atoms/CardCertificate/CardCertificate.js
+++ src/components/atoms/CardCertificate/CardCertificate.js
@@ -1,4 +1,5 @@
 import { html } from '@polymer/lit-element';
+import CSS from './_components.card-css';
 
 export default function CardCertificate ({
   hasCertificateDefinition,
@@ -14,13 +15,13 @@ export default function CardCertificate ({
     return null;
   }
 
-  console.log(issuedOn);
-
-  return html`<section>
-    <img src='${issuerLogo}' alt='${issuerName}'/>
-    <h1>${certificateTitle}</h1>
-    <h2>${recipientName}</h2>
-    <span>Issued on <time datetime$='${issuedOn}'>${issueDate}</time> by ${issuerName}</span>
-    <a href='${recordLink}' target='_blank'>View Record ></a>
-  </section>`;
+  return html`
+    ${CSS}
+    <section class='buv-c-card'>
+      <img src='${issuerLogo}' alt='${issuerName}' class='buv-c-card__img'/>
+      <h1 class='buv-c-card__title'>${certificateTitle}</h1>
+      <h2 class='buv-c-card__title  buv-c-card__recipient'>${recipientName}</h2>
+      <span class='buv-c-card__issue-summary  buv-c-card__subtext'>Issued on <time datetime$='${issuedOn}'>${issueDate}</time> by ${issuerName}</span>
+      <a class='buv-c-card__subtext  buv-c-card__record-link' href='${recordLink}' target='_blank'>View Record</a>
+    </section>`;
 }
