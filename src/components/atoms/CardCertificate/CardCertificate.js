cfd308a73b75c8d230b4aa660fb576707587f85a
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-09 14:05:06 +0200

    feat(Card): display information

--- src/components/atoms/CardCertificate/CardCertificate.js
+++ src/components/atoms/CardCertificate/CardCertificate.js
@@ -1,5 +1,24 @@
 import { html } from '@polymer/lit-element';
 
 export default function CardCertificate ({ certificateDefinition }) {
-  return html`<div>${certificateDefinition}</div>`;
+  if (!certificateDefinition) {
+    return null;
+  }
+  // TODO: move that away from component, to selector
+  const recipientName = certificateDefinition.recipientProfile.name;
+  const certificateTitle = certificateDefinition.badge.name;
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  const objDate = new Date(certificateDefinition.issuedOn);
+  const issueDate = months[objDate.getMonth()] + ' ' + objDate.getDate() + ', ' + objDate.getFullYear();
+  const issuerName = certificateDefinition.badge.issuer.name;
+  const issuerLogo = certificateDefinition.badge.issuer.image;
+  const recordLink = certificateDefinition.id;
+
+  return html`<section>
+    <img src='${issuerLogo}' alt='${issuerName}'/>
+    <h1>${certificateTitle}</h1>
+    <h2>${recipientName}</h2>
+    <span>Issued on ${issueDate} by ${issuerName}</span>
+    <a href='${recordLink}' target='_blank'>View Record ></a>
+  </section>`;
 }
