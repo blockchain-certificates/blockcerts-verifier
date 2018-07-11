a6934a7007a57e67628fde12462c6510f7a7e66a
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-11 13:12:08 +0200

    feat(AllowDownload): add styles

--- src/components/atoms/DownloadLink/DownloadLink.js
+++ src/components/atoms/DownloadLink/DownloadLink.js
@@ -1,4 +1,5 @@
 import html from '@polymer/lit-element';
+import CSS from './_components.download-link-css';
 
 export default function DownloadLink (allowDownload, certificateDownloadLink) {
   if (!allowDownload || !certificateDownloadLink) {
@@ -6,6 +7,7 @@ export default function DownloadLink (allowDownload, certificateDownloadLink) {
   }
 
   return html`
+    ${CSS}
     <a href='${certificateDownloadLink}' title='Download Record (JSON format)'>
       <span class='buv-u-visually-hidden'>Download Record in JSON format</span>
     </a>`;
