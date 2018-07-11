6263370e4950e77c56001ecbd7549db228f22812
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-11 14:26:05 +0200

    feat(DownloadLink): proper display and tests

--- src/components/atoms/DownloadLink/DownloadLink.js
+++ src/components/atoms/DownloadLink/DownloadLink.js
@@ -1,14 +1,18 @@
 import { html } from '@polymer/lit-element';
 import CSS from './_components.download-link-css';
 
-export default function DownloadLink (allowDownload, certificateDownloadLink) {
-  if (!allowDownload || !certificateDownloadLink) {
+export default function DownloadLink ({ allowDownload, downloadLink }) {
+  if (!allowDownload) {
+    return null;
+  }
+
+  if (!downloadLink) {
     return null;
   }
 
   return html`
     ${CSS}
-    <a href='${certificateDownloadLink}' title='Download Record (JSON format)'>
+    <a class='buv-c-download-link  buv-qa-download-link' href='${downloadLink}' title='Download Record (JSON format)'>
       <span class='buv-u-visually-hidden'>Download Record in JSON format</span>
     </a>`;
 }
