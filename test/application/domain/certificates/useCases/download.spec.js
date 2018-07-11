bcf88dd11919d3561af280c89388b60fd4c04445
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-11 14:27:28 +0200

    style(Lint): lint

--- test/application/domain/certificates/useCases/download.spec.js
+++ test/application/domain/certificates/useCases/download.spec.js
@@ -5,7 +5,7 @@ describe('domain certificates download method test suite', function () {
     it('should return the URL with the formatJson option appended', function () {
       const url = 'https://auto-certificates.learningmachine.io/certificate/0cc5fb04900856b4aeb3733af2e019df';
       const expectedOutput = url + '?format=json';
-      expect(domain.certificates.download(url)).toBe(expectedOutput)
+      expect(domain.certificates.download(url)).toBe(expectedOutput);
     });
   });
 });
