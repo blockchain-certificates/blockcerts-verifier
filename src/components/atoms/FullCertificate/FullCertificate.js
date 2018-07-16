3c5e9ebf1ae73df88532911e5642e902959729f5
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-16 15:53:06 +0200

    feat(FullCertificate): retrieve and display extra data

--- src/components/atoms/FullCertificate/FullCertificate.js
+++ src/components/atoms/FullCertificate/FullCertificate.js
@@ -2,7 +2,15 @@ import { html } from '@polymer/lit-element';
 import { unsafeHTML } from 'lit-html/lib/unsafe-html.js';
 import CSS from './_components.full-certificate-css';
 
-export default function CardCertificate ({ hasCertificateDefinition, displayHTML }) {
+export default function CardCertificate ({
+    hasCertificateDefinition,
+    displayHTML,
+    recipientName,
+    issuedOn,
+    issueDate,
+    issuerName,
+    transactionLink
+  }) {
   if (!hasCertificateDefinition) {
     return null;
   }
@@ -10,5 +18,15 @@ export default function CardCertificate ({ hasCertificateDefinition, displayHTML
   return html`
     ${CSS}
     <div class='buv-c-full-certificate'>${unsafeHTML(displayHTML)}</div>
+    <dl class='buv-c-full-certificate-details'>
+        <dt>Recipient</dt>
+        <dd>${recipientName}</dd>
+        <dt>Issue Date</dt>
+        <dd><time datetime$='${issuedOn}'>${issueDate}</time></dd>
+        <dt>Issuer</dt>
+        <dd>${issuerName}</dd>
+        <dt>Transaction Id</dt>
+        <dd>${transactionLink}</dd>
+    </dl>
   `;
 }
--- src/components/atoms/FullCertificate/FullCertificateContainer.js
+++ src/components/atoms/FullCertificate/FullCertificateContainer.js
@@ -1,10 +1,25 @@
 import connector from '../../../store/connector';
 import FullCertificate from './FullCertificate';
-import { getCertificateDefinition, getDisplayHTML } from '../../../selectors/certificate';
+import {
+  getCertificateDefinition,
+  getDisplayHTML,
+  getIssueDate,
+  getIssuedOn,
+  getIssuerLogo,
+  getIssuerName,
+  getRecipientName,
+  getTransactionLink
+} from '../../../selectors/certificate';
 
 const mapStateToProps = (state) => ({
   hasCertificateDefinition: !!getCertificateDefinition(state),
-  displayHTML: getDisplayHTML(state)
+  displayHTML: getDisplayHTML(state),
+  recipientName: getRecipientName(state),
+  issueDate: getIssueDate(state),
+  issuedOn: getIssuedOn(state),
+  issuerName: getIssuerName(state),
+  issuerLogo: getIssuerLogo(state),
+  transactionLink: getTransactionLink(state),
 });
 
 const FullCertificateContainer = connector(FullCertificate, { mapStateToProps });
