7c14ae381914aef1c1ed37216f009520205ed38f
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-17 13:02:23 +0200

    feat(Metadata): display metadata list

--- src/components/atoms/Metadata/Metadata.js
+++ src/components/atoms/Metadata/Metadata.js
@@ -1,6 +1,20 @@
 import { html } from '@polymer/lit-element';
 
 export default function Metadata ({ metadataList }) {
-  console.log(metadataList);
-  return html`<section>${metadataList}</section>`
+  if (!metadataList) {
+    return null;
+  }
+
+  const innerHTML = metadataList.displayOrder.map(entry => {
+    const key = entry.split('.')[1]; // get key name
+    const title = metadataList.schema.properties.certificate.properties[key].title;
+    const value = metadataList.certificate[key];
+
+    return html`
+      <dt>${title}</dt>
+      <dd>${value}</dd>
+    `;
+  });
+
+  return html`<section><dl>${innerHTML}</dl></section>`;
 }
--- src/components/atoms/Metadata/MetadataContainer.js
+++ src/components/atoms/Metadata/MetadataContainer.js
@@ -1,12 +1,8 @@
 import connector from '../../../store/connector';
 import Metadata from './Metadata';
-import {
-  getCertificateDefinition,
-  getMetadataJson
-} from '../../../selectors/certificate';
+import { getMetadataJson } from '../../../selectors/certificate';
 
 const mapStateToProps = (state) => ({
-  hasCertificateDefinition: !!getCertificateDefinition(state),
   metadataList: getMetadataJson(state)
 });
 
