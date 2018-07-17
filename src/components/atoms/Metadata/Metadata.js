bcf837a8e131bb9eb0600651ae3320c79d01b0c2
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-17 14:37:39 +0200

    refactor(Metadata): read only properties chain once

--- src/components/atoms/Metadata/Metadata.js
+++ src/components/atoms/Metadata/Metadata.js
@@ -1,14 +1,20 @@
 import { html } from '@polymer/lit-element';
 import getValueFrom from '../../../helpers/getValueFrom';
 
+function getProperties (metadataList) {
+  return metadataList.schema.properties.certificate.properties
+}
+
 export default function Metadata ({ metadataList }) {
   if (!metadataList) {
     return null;
   }
 
+  const properties = getProperties(metadataList);
+
   const innerHTML = metadataList.displayOrder.map(entry => {
     const key = entry.split('.')[1]; // get key name
-    const title = metadataList.schema.properties.certificate.properties[key].title;
+    const title = properties[key].title;
     const value = getValueFrom(metadataList, entry);
 
     return html`
