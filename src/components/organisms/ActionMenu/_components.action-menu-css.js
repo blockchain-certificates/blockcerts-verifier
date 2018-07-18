dc0348a9cff80ca2da06dc2a2365c8ce8c689e55
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-18 11:54:13 +0200

    feat(ActionMenu): make display adaptative to content

--- src/components/organisms/ActionMenu/_components.action-menu-css.js
+++ src/components/organisms/ActionMenu/_components.action-menu-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-c-action-menu{display:flex;width:80px;justify-content:space-between;align-items:center;padding:0}
+export default html`<style>.buv-c-action-menu{display:flex;align-items:center;padding:0}.buv-c-action-menu-item{flex-basis:30px}
 </style>`;
