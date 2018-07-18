67771ba0fb7a75e4815ca452fbdd6bd275672e71
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-18 10:53:15 +0200

    feat(ActionMenu): style component

--- src/components/organisms/ActionMenu/ActionMenu.js
+++ src/components/organisms/ActionMenu/ActionMenu.js
@@ -2,9 +2,11 @@ import { html } from '@polymer/lit-element';
 import '../../atoms/DownloadLink';
 import '../../molecules/Metadata';
 import '../../molecules/SocialShare';
+import CSS from './_components.action-menu-css';
 
 const ActionMenu = html`
-  <menu>
+  ${CSS}
+  <menu class='buv-c-action-menu'>
     <menuitem><buv-metadata></buv-metadata></menuitem>
     <menuitem><buv-download-link></buv-download-link></menuitem>
     <menuitem><buv-social-share></buv-social-share></menuitem>
---
+++ src/components/organisms/ActionMenu/_components.action-menu-css.js
@@ -0,0 +1,3 @@
+import {html} from '@polymer/lit-element';
+export default html`<style>.buv-c-action-menu{display:flex;width:80px;justify-content:space-between;align-items:center;padding:0}
+</style>`;
