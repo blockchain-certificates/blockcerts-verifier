09bc58078f34127d68ced4183501d004c456b3ab
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-19 18:21:10 +0200

    refactor(CertificateInput): follow components architecture convention

--- src/components/organisms/CertificateInput/index.js
+++ src/components/organisms/CertificateInput/index.js
@@ -1,16 +1,3 @@
-import { html } from '@polymer/lit-element';
-import '../../atoms/Input';
-import '../../atoms/FileUpload';
-import '../../molecules/Button';
-
-import CSS from './_components.certificate-input-css';
-
-const CertificateInput = html`
-    ${CSS}
-    <section class="buv-c-certificate-input">
-        <buv-input class="buv-c-certificate-input__input"></buv-input><buv-button></buv-button>     
-    </section>
-    <buv-file-upload></buv-file-upload>   
-`;
+import CertificateInput from './CertificateInput';
 
 export default CertificateInput;
