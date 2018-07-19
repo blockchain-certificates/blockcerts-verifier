bdb7b6bde61ee3b82a9a629c008adb089a1e2181
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-19 14:55:05 +0200

    feat(All): small polish

--- src/components/atoms/FileUpload/_components.file-upload-css.js
+++ src/components/atoms/FileUpload/_components.file-upload-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-c-file-upload{text-decoration:underline;cursor:pointer}.buv-u-visually-hidden{position:absolute !important;clip:rect(1px 1px 1px 1px);clip:rect(1px, 1px, 1px, 1px);padding:0 !important;border:0 !important;height:1px !important;width:1px !important;overflow:hidden}
+export default html`<style>.buv-o-link{color:#031532}.buv-o-small-text{font-size:12px}.buv-o-large-text{font-size:15px}.buv-c-file-upload{text-decoration:underline;cursor:pointer}.buv-u-visually-hidden{position:absolute !important;clip:rect(1px 1px 1px 1px);clip:rect(1px, 1px, 1px, 1px);padding:0 !important;border:0 !important;height:1px !important;width:1px !important;overflow:hidden}
 </style>`;
