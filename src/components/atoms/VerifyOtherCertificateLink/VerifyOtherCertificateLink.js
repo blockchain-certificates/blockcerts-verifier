7ad63b3ec16fbeb78e42c866654f71a350a04056
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-20 10:08:18 +0200

    feat(VerifyOtherCertificate): style component

--- src/components/atoms/VerifyOtherCertificateLink/VerifyOtherCertificateLink.js
+++ src/components/atoms/VerifyOtherCertificateLink/VerifyOtherCertificateLink.js
@@ -1,9 +1,13 @@
 import { html } from '@polymer/lit-element';
+import CSS from './_components.verify-other-certificate-css';
 
 export default function VerifyOtherCertificateLink ({ onClick = () => {}, isVisible = false } = {}) {
   if (!isVisible) {
     return null;
   }
 
-  return html`<a onclick='${onClick}'>Verify another record</a>`
+  return html`
+    ${CSS}
+    <a onclick='${onClick}' class='buv-o-small-text buv-o-link'>Verify another record</a>
+  `
 }
