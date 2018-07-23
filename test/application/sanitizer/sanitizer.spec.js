fa8b17b8c85ba351034091716a5769695ef9cf17
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-23 13:26:40 +0200

    refactor(Test): reorganize tests for better semantics

--- test/application/sanitizer/sanitizer.spec.js
+++ test/application/sanitizer/sanitizer.spec.js
@@ -4,67 +4,71 @@ describe('sanitizer test suite', function () {
   // TODO: we are only testing the most obvious cases, but could do more
   // https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet
   // https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet
-  it('should remove script tags from a definition', function () {
-    const html = '<section><h1>Testing</h1><script>alert("XSS")</script></section>';
+  describe('should remove', function () {
+    it('script tags', function () {
+      const html = '<section><h1>Testing</h1><script>alert("XSS")</script></section>';
 
-    const expectedOutput = '<section><h1>Testing</h1></section>';
+      const expectedOutput = '<section><h1>Testing</h1></section>';
 
-    expect(sanitize(html)).toBe(expectedOutput);
+      expect(sanitize(html)).toBe(expectedOutput);
+    });
+
+    it('style tags', function () {
+      const html = '<section><h1>Testing</h1><style>alert("XSS")</style></section>';
+
+      const expectedOutput = '<section><h1>Testing</h1></section>';
+
+      expect(sanitize(html)).toBe(expectedOutput);
+    });
+
+    it('event attributes', function () {
+      const html = '<section><h1>Testing</h1><div onclick="javascript:alert(\'yo\')">Yo</div></section>';
+
+      const expectedOutput = '<section><h1>Testing</h1><div>Yo</div></section>';
+
+      expect(sanitize(html)).toBe(expectedOutput);
+    });
+
+    it('dangerous instructions from a style attribute', function () {
+      const html = '<section><h1>Testing</h1><div style="display:inline;background:url(java/**/script:alert(1)">Yo</div></section>';
+
+      const expectedOutput = '<section><h1>Testing</h1><div style="display:inline;">Yo</div></section>';
+
+      expect(sanitize(html)).toBe(expectedOutput);
+    });
   });
 
-  it('should remove style tags from a definition', function () {
-    const html = '<section><h1>Testing</h1><style>alert("XSS")</style></section>';
+  describe('should keep', function () {
+    it('style attributes', function () {
+      const html = '<section><h1>Testing</h1><div style="display:inline;">Yo</div></section>';
 
-    const expectedOutput = '<section><h1>Testing</h1></section>';
+      const expectedOutput = '<section><h1>Testing</h1><div style="display:inline;">Yo</div></section>';
 
-    expect(sanitize(html)).toBe(expectedOutput);
-  });
+      expect(sanitize(html)).toBe(expectedOutput);
+    });
 
-  it('should remove event attributes from a definition', function () {
-    const html = '<section><h1>Testing</h1><div onclick="javascript:alert(\'yo\')">Yo</div></section>';
+    it('class attributes', function () {
+      const html = '<section><h1>Testing</h1><div class="content">Yo</div></section>';
 
-    const expectedOutput = '<section><h1>Testing</h1><div>Yo</div></section>';
+      const expectedOutput = '<section><h1>Testing</h1><div class="content">Yo</div></section>';
 
-    expect(sanitize(html)).toBe(expectedOutput);
-  });
+      expect(sanitize(html)).toBe(expectedOutput);
+    });
 
-  it('should keep style attributes from a definition', function () {
-    const html = '<section><h1>Testing</h1><div style="display:inline;">Yo</div></section>';
+    it('valid href attribute', function () {
+      const html = '<section><h1>Testing</h1><div><a href="http://lm.com/yo.html">Yo</a></div></section>';
 
-    const expectedOutput = '<section><h1>Testing</h1><div style="display:inline;">Yo</div></section>';
+      const expectedOutput = '<section><h1>Testing</h1><div><a href="http://lm.com/yo.html">Yo</a></div></section>';
 
-    expect(sanitize(html)).toBe(expectedOutput);
-  });
+      expect(sanitize(html)).toBe(expectedOutput);
+    });
 
-  it('should filter style attributes from a definition', function () {
-    const html = '<section><h1>Testing</h1><div style="display:inline;background:url(java/**/script:alert(1)">Yo</div></section>';
+    it('valid src attribute', function () {
+      const html = '<section><h1>Testing</h1><div><img src="http://lm.com/yo.jpg" alt="MultiYO" />Yo</div></section>';
 
-    const expectedOutput = '<section><h1>Testing</h1><div style="display:inline;">Yo</div></section>';
+      const expectedOutput = '<section><h1>Testing</h1><div><img src="http://lm.com/yo.jpg" alt="MultiYO" />Yo</div></section>';
 
-    expect(sanitize(html)).toBe(expectedOutput);
-  });
-
-  it('should keep class attributes from a definition', function () {
-    const html = '<section><h1>Testing</h1><div class="content">Yo</div></section>';
-
-    const expectedOutput = '<section><h1>Testing</h1><div class="content">Yo</div></section>';
-
-    expect(sanitize(html)).toBe(expectedOutput);
-  });
-
-  it('should keep valid href attribute from a definition', function () {
-    const html = '<section><h1>Testing</h1><div><a href="http://lm.com/yo.html">Yo</a></div></section>';
-
-    const expectedOutput = '<section><h1>Testing</h1><div><a href="http://lm.com/yo.html">Yo</a></div></section>';
-
-    expect(sanitize(html)).toBe(expectedOutput);
-  });
-
-  it('should keep valid src attribute from a definition', function () {
-    const html = '<section><h1>Testing</h1><div><img src="http://lm.com/yo.jpg" alt="MultiYO" />Yo</div></section>';
-
-    const expectedOutput = '<section><h1>Testing</h1><div><img src="http://lm.com/yo.jpg" alt="MultiYO" />Yo</div></section>';
-
-    expect(sanitize((html))).toBe(expectedOutput);
+      expect(sanitize((html))).toBe(expectedOutput);
+    });
   });
 });
