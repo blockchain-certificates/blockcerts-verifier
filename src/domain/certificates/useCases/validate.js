b15328e082664acab2c3811364b8700086aeae8b
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-25 14:06:50 +0200

    chore(UpdateVerifier): fix verification process tests

--- src/domain/certificates/useCases/validate.js
+++ src/domain/certificates/useCases/validate.js
@@ -1,9 +1,10 @@
-import { Certificate } from 'cert-verifier-js/verifier-es';
+import { Certificate } from 'cert-verifier-js/dist/verifier-es';
 
 export default function validate (definition) {
   let isValid = false;
   try {
-    isValid = !!Certificate.parseJson(definition);
+    const certificate = new Certificate(definition);
+    isValid = true;
 
     return {
       isValid
