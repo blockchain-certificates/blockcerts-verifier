d524148d15b317a3e204b2b1282f91400b9a36c0
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-25 16:28:05 +0200

    refactor(Certificate): remove unused domain code

--- src/domain/certificates/useCases/index.js
+++ src/domain/certificates/useCases/index.js
@@ -3,13 +3,11 @@ import retrieve from './retrieve';
 import parseDefinition from './parseDefinition';
 import read from './read';
 import validate from './validate';
-import verify from './verify';
 
 export {
   download,
   retrieve,
   parseDefinition,
   read,
-  validate,
-  verify
+  validate
 };
