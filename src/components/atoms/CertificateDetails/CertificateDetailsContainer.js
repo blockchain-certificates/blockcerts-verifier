dfdd0a9fb54330f6cc30d5f224eee055cb3c0826
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-26 13:41:56 +0200

    feat(CertificateDetails): introduce component to handle certificate details

---
+++ src/components/atoms/CertificateDetails/CertificateDetailsContainer.js
@@ -0,0 +1,24 @@
+import connector from '../../../store/connector';
+import CertificateDetails from './CertificateDetails';
+import {
+  getIssueDate,
+  getIssuedOn,
+  getIssuerLogo,
+  getIssuerName,
+  getRecipientName,
+  getTransactionId,
+  getTransactionLink
+} from '../../../selectors/certificate';
+
+export const mapStateToProps = (state) => ({
+  recipientName: getRecipientName(state),
+  issueDate: getIssueDate(state),
+  issuedOn: getIssuedOn(state),
+  issuerName: getIssuerName(state),
+  issuerLogo: getIssuerLogo(state),
+  transactionLink: getTransactionLink(state),
+  transactionId: getTransactionId(state)
+});
+
+const CertificateDetailsContainer = connector(CertificateDetails, { mapStateToProps });
+export { CertificateDetailsContainer };
