ef98bb7780591710d8ecca2fbee77c6d5de1ab12
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-30 13:41:59 +0200

    refactor(VerificationProcess): simplify stepVerified reducer

--- src/reducers/stepVerified.js
+++ src/reducers/stepVerified.js
@@ -1,28 +1,14 @@
-import { getVerifiedSteps, getParentStep } from '../selectors/certificate';
+import { getParentStep } from '../selectors/certificate';
 
-function saveSubstepIn (parent, substep) {
-  // does substep exist in parent yet?
-  // TODO: // TODO: assess if should remove? substep should always be defined initially
+function updateSubstepIn (parent, substep) {
   const substepIndex = parent.subSteps.findIndex(s => s.code === substep.code);
-
-  if (substepIndex > -1) {
-    parent.subSteps[substepIndex] = substep;
-  } else {
-    // TODO: assess if should remove? We should never get there.
-    parent.subSteps.push(substep);
-  }
+  parent.subSteps[substepIndex] = substep;
 }
 
 export default function stepVerified (state, action) {
   const { parentStep } = action.payload;
   const storedParentState = getParentStep(state, parentStep);
-
-  if (parentStep && storedParentState) {
-    saveSubstepIn(storedParentState, action.payload);
-  } else {
-    // TODO: assess if should remove? We should never get there.
-    getVerifiedSteps(state).push(action.payload);
-  }
+  updateSubstepIn(storedParentState, action.payload);
 
   return state;
 }
