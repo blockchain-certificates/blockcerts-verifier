33e641571140d82d0b70cd8cd4ef6df898881b41
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-30 16:11:22 +0200

    feat(VerificationModal): add close button to Modal

--- src/components/atoms/Modal/Modal.js
+++ src/components/atoms/Modal/Modal.js
@@ -1,11 +1,15 @@
 import { html, LitElement } from '@polymer/lit-element';
 import CSS from './_components.modal-css';
+import CloseButton from '../CloseButton/';
 
 class Modal extends LitElement {
   _render () {
     return html`
       ${CSS}
       <div class='buv-c-modal  buv-o-overlay'>
+        ${CloseButton({
+       className: 'buv-c-modal__close-button'
+    })}
         <slot></slot>
       </div>
     `;
