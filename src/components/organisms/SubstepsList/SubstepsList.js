c960558dd4571c8395b733aae6a4a645f399530b
Julien Fraichot <jfraichot@learningmachine.com>
2018-08-01 11:59:58 +0200

    feat(VerificationProcess): enable show/hide of substeps

--- src/components/organisms/SubstepsList/SubstepsList.js
+++ src/components/organisms/SubstepsList/SubstepsList.js
@@ -2,19 +2,32 @@ import { html, LitElement } from '@polymer/lit-element';
 import VerificationStep from '../../molecules/VerificationStep/index';
 
 class SubStepsList extends LitElement {
+  constructor () {
+    super ();
+    this.isOpen = false;
+    this.toggleOpen = this.toggleOpen.bind(this);
+  }
   static get properties () {
     return {
-      subSteps: []
+      subSteps: [],
+      isOpen: Boolean
     }
   }
 
+  toggleOpen () {
+    this.isOpen = !this.isOpen;
+  }
+
+  showList (subSteps) {
+    return html`${ subSteps.map(subStep => html`${VerificationStep(subStep)}`) }`;
+  }
+
   _render (props) {
-    console.log(props);
     return html`
-    ${props.subSteps
-      .filter(subStep => !!subStep.status)
-      .map(subStep => html`${VerificationStep(subStep)}`)
-    }
+    <a title='Toggle open list of substeps' onclick='${this.toggleOpen}'>
+      ${this.isOpen ? 'hide' : `${props.subSteps.length} items`}
+    </a>
+    ${this.isOpen ? this.showList(props.subSteps) : null}
     `;
   }
 }
