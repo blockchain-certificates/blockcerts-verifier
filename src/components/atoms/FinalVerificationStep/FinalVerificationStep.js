bcbcc1280d987e57a898a05a216723b076ab8fbb
Julien Fraichot <jfraichot@learningmachine.com>
2018-08-01 10:54:20 +0200

    feat(MocknetVerification): adapt copy of final step

--- src/components/atoms/FinalVerificationStep/FinalVerificationStep.js
+++ src/components/atoms/FinalVerificationStep/FinalVerificationStep.js
@@ -12,11 +12,17 @@ export default function FinalVerificationStep ({ hasError = false, chain = '', t
     isTestChain ? 'is-test' : ''
   ].join(' ');
 
+  const title = isTestChain ? 'This Mocknet credential passed all checks' : 'Verified';
+  const details = isTestChain ?
+    'Mocknet credentials are used for test purposes only. They are not recorded on a blockchain, ' +
+    'and they should not be considered verified Blockcerts.':
+    html`This is a valid ${chain} certificate.<br/>
+      <a class='buv-o-link' href='${transactionLink}' hidden?='${!transactionLink}'>View transaction link</a>`;
+
   return html`
-    <dt class$='${classes}'>Verified</dt>
+    <dt class$='${classes}'>${title}</dt>
     <dd class='buv-c-verification-substep  buv-u-excluded-from-flow  buv-o-small-text'>
-      This is a valid ${chain} certificate.<br/>
-      <a class='buv-o-link' href='${transactionLink}' hidden?='${!transactionLink}'>View transaction link</a>
+      ${details}
     </dd>
   `;
 }
