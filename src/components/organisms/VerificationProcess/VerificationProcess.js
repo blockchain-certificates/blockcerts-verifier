2d29552d6fa10c02f0e03826292c09a001fd83fa
Julien Fraichot <jfraichot@learningmachine.com>
2018-08-06 15:59:39 +0200

    style(Lint): lint

--- src/components/organisms/VerificationProcess/VerificationProcess.js
+++ src/components/organisms/VerificationProcess/VerificationProcess.js
@@ -20,7 +20,7 @@ class VerificationProcess extends LitElement {
     return html`
         <span class='buv-u-visually-hidden'>Verifying step...</span>
         <svg width='20' height='7' viewBox='0 0 120 30' xmlns='http://www.w3.org/2000/svg'><circle cx='15' cy='15' r='15'><animate attributeName='r' from='15' to='15' begin='0s' dur='0.8s' values='15;9;15' calcMode='linear' repeatCount='indefinite'/><animate attributeName='fill-opacity' from='1' to='1' begin='0s' dur='0.8s' values='1;.5;1' calcMode='linear' repeatCount='indefinite'/></circle><circle cx='60' cy='15' r='9' fill-opacity=''.9'><animate attributeName='r' from='9' to='9' begin='0s' dur='0.8s' values='9;15;9' calcMode='linear' repeatCount='indefinite'/><animate attributeName='fill-opacity' from=''.5' to='.5' begin='0s' dur='0.8s' values='.5;1;.5' calcMode='linear' repeatCount='indefinite'/></circle><circle cx='105' cy='15' r='15'><animate attributeName='r' from='15' to='15' begin='0s' dur='0.8s' values='15;9;15' calcMode='linear' repeatCount='indefinite'/><animate attributeName='fill-opacity' from='1' to='1' begin='0s' dur='0.8s' values='1;.5;1' calcMode='linear' repeatCount='indefinite'/></circle></svg>
-    `
+    `;
   }
 
   _didRender () {
