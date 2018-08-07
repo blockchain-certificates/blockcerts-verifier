a018821ae1bbcdec41ab2dcad0afc0b154b1c32c
Julien Fraichot <jfraichot@learningmachine.com>
2018-08-07 14:33:21 +0200

    fix(VerificationProcess): maintain display alignment of process

--- src/components/organisms/VerificationModal/_components.verification-modal-css.js
+++ src/components/organisms/VerificationModal/_components.verification-modal-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-c-verification-modal__certificate{margin-bottom:30px;display:block}.buv-c-verification-modal__process{display:flex;justify-content:center}.buv-c-verification-modal__separator{margin-bottom:30px;border-color:rgba(3,21,50,0.05)}
+export default html`<style>.buv-c-verification-modal__certificate{margin-bottom:30px;display:block}.buv-c-verification-modal__process{display:flex;width:50%;margin:0 auto}.buv-c-verification-modal__separator{margin-bottom:30px;border-color:rgba(3,21,50,0.05)}
 </style>`;
