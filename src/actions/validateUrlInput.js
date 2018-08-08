8c24e7d6196712608f6a30b7c3babe6b9c3af74f
Julien Fraichot <jfraichot@learningmachine.com>
2018-08-08 13:19:31 +0200

    feat(ValidateURL): show error message when URL is invalid

--- src/actions/validateUrlInput.js
+++ src/actions/validateUrlInput.js
@@ -1,10 +1,16 @@
 import * as ACTIONS from '../constants/actionTypes';
+import setErrorMessage from './setErrorMessage';
 
 export default function validateUrlInput (isValid) {
-  return {
-    type: ACTIONS.VALIDATE_URL_INPUT,
-    payload: {
-      isValid
-    }
+  return function (dispatch) {
+    dispatch({
+      type: ACTIONS.VALIDATE_URL_INPUT,
+      payload: {
+        isValid
+      }
+    });
+
+    const errorMessage = isValid ? null : 'This does not seem to be a valid URL.';
+    dispatch(setErrorMessage(errorMessage));
   };
 }
