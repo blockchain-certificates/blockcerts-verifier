da5054dee345e20de46b1fc604365ef23d06e371
Julien Fraichot <jfraichot@learningmachine.com>
2018-08-09 13:22:58 +0200

    feat(Branding) : add branding footer

---
+++ src/components/atoms/Footer/_components.footer-css.js
@@ -0,0 +1,3 @@
+import {html} from '@polymer/lit-element';
+export default html`<style>.buv-c-footer{width:100%;height:60px;background-color:rgba(3,21,50,0.9);color:#fff;display:flex;justify-content:flex-end;align-items:center;padding:0 20px;box-sizing:border-box;position:absolute;bottom:0;left:0}.buv-u-visually-hidden{position:absolute !important;clip:rect(1px 1px 1px 1px);clip:rect(1px, 1px, 1px, 1px);padding:0 !important;border:0 !important;height:1px !important;width:1px !important;overflow:hidden}
+</style>`;
