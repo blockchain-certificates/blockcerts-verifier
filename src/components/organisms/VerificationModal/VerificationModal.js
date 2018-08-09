bea430678972276d0bcbd596d4d632eaf779e081
Julien Fraichot <jfraichot@learningmachine.com>
2018-08-09 16:51:48 +0200

    feat(Modal): ensure footer modal is always at bottom even with overflow

--- src/components/organisms/VerificationModal/VerificationModal.js
+++ src/components/organisms/VerificationModal/VerificationModal.js
@@ -8,9 +8,11 @@ function VerificationModal ({ isOpen, onClose }) {
   return html`
     ${CSS}
     <buv-modal isOpen?='${isOpen}' onClose='${onClose}'>
-      <buv-card-certificate class='buv-c-verification-modal__certificate' hideRecordLink hideVerifyButton></buv-card-certificate>
-      <hr class='buv-c-verification-modal__separator'/>
-      <buv-verification-process class='buv-c-verification-modal__process'></buv-verification-process>
+      <div class='buv-c-verification-modal__content'>
+        <buv-card-certificate class='buv-c-verification-modal__certificate' hideRecordLink hideVerifyButton></buv-card-certificate>
+        <hr class='buv-c-verification-modal__separator'/>
+        <buv-verification-process class='buv-c-verification-modal__process'></buv-verification-process>
+      </div>
     ${Footer({ forceInPlace: true })}
     </buv-modal>
   `;
--- src/components/organisms/VerificationModal/_components.verification-modal-css.js
+++ src/components/organisms/VerificationModal/_components.verification-modal-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-c-verification-modal__certificate{margin-bottom:30px;display:block}.buv-c-verification-modal__process{display:flex;justify-content:space-around}.buv-c-verification-modal__separator{margin-bottom:30px;border-color:rgba(3,21,50,0.05)}
+export default html`<style>.buv-c-verification-modal__certificate{margin-bottom:30px;display:block}.buv-c-verification-modal__process{display:flex;justify-content:space-around}.buv-c-verification-modal__separator{margin-bottom:30px;border-color:rgba(3,21,50,0.05)}.buv-c-verification-modal__content{max-height:calc(100vh - 38px - 76px - 60px);padding-bottom:50px;box-sizing:border-box;overflow:auto}
 </style>`;
