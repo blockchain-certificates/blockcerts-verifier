a90ef2473853b194f595495e5bbb782a502dc166
Julien Fraichot <jfraichot@learningmachine.com>
2018-08-13 19:42:24 +0200

    feat(Tracking): view to trigger socialShare event

--- src/components/molecules/SocialShare/SocialShareContainer.js
+++ src/components/molecules/SocialShare/SocialShareContainer.js
@@ -1,10 +1,15 @@
 import connector from '../../../store/connector';
 import { SocialShare } from './SocialShare';
 import { getRecordLink } from '../../../selectors/certificate';
+import shareSocialNetwork from '../../../actions/shareSocialNetwork';
+
+const mapDispatchToProps = {
+  onShare: shareSocialNetwork
+};
 
 export const mapStateToProps = (state) => ({
   url: getRecordLink(state)
 });
 
-const SocialShareContainer = connector(SocialShare, { mapStateToProps });
+const SocialShareContainer = connector(SocialShare, { mapDispatchToProps, mapStateToProps });
 export { SocialShareContainer };
