7ad53e45851c9470464ee0785e50b5cfb746bd87
Matthieu COLLÉ <druidev+github@gmail.com>
2018-08-17 22:30:38 +0200

    feat(design): more design changes

--- src/components/atoms/DragAndDrop/_components.drag-and-drop-css.js
+++ src/components/atoms/DragAndDrop/_components.drag-and-drop-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-o-link{color:#031532}.buv-o-small-text{font-size:12px}.buv-o-large-text{font-size:15px}.buv-c-drag-and-drop__droparea{z-index:100;background-color:rgba(0,0,0,0.1);display:none;position:fixed;top:0;left:0;width:100%;height:100%}.buv-c-drag-and-drop__droparea.is-active{display:block}
+export default html`<style>.buv-o-link{color:#031532}.buv-o-small-text{font-size:12px}.buv-o-large-text{font-size:15px;line-height:20px}.buv-c-drag-and-drop__droparea{z-index:100;background-color:rgba(0,0,0,0.1);display:none;position:fixed;top:0;left:0;width:100%;height:100%}.buv-c-drag-and-drop__droparea.is-active{display:block}
 </style>`;
