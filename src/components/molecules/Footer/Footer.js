c40210cc295985718d62be5871f2f4c6a70ee817
Matthieu COLLÉ <druidev+github@gmail.com>
2018-08-20 20:16:03 +0200

    feat(Footer): moved footer into molecules

---
+++ src/components/molecules/Footer/Footer.js
@@ -0,0 +1,22 @@
+import '../../atoms/FileUpload';
+import { html } from '@polymer/lit-element';
+import CSS from './_components.footer-css';
+import BlockcertsLogo from '../../atoms/BlockcertsLogo/BlockcertsLogo';
+
+const Footer = ({forceInPlace = false}) => {
+  const classes = [
+    'buv-c-footer',
+    forceInPlace ? 'buv-c-footer--forced' : ''
+  ].join(' ');
+
+  return html`
+  ${CSS}
+  <footer class$='${classes}'>
+    <section>
+      <buv-file-upload></buv-file-upload>
+    </section>
+    ${BlockcertsLogo()}
+  </footer>`;
+};
+
+export default Footer;
