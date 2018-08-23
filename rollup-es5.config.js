3a6a93e1d2eb6031961538283cc8e8e666dc8555
Matthieu COLLÉ <druidev+github@gmail.com>
2018-08-23 17:25:49 -0500

    chore(BuildProcess): added es5 build

--- rollup-es5.config.js
+++ rollup-es5.config.js
@@ -1,16 +1,22 @@
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/blockcerts-universal-verifier/index.js',
   output: [
     {
-      file: 'dist/index.js',
+      file: 'dist/buv-iife.js',
       format: 'iife',
       name: 'BlockcertsUniversalVerifier'
     }
   ],
   plugins: [
+    babel({
+      babelrc: false,
+      presets: [['env', { modules: false }]],
+      plugins: ['external-helpers', 'transform-object-rest-spread']
+    }),
     replace({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
