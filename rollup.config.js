4e78c1c7dbc2aa7c0606b868c4a0a5b55481d8a6
Matthieu COLLÉ <druidev+github@gmail.com>
2018-08-24 02:45:53 +0200

    chore(BuildProcess): rollup build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,26 @@
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/blockcerts-universal-verifier/index.js',
   output: [
     {
-      file: 'dist/buv-iife.js',
+      file: 'dist/buv.js',
       format: 'iife',
       name: 'BlockcertsUniversalVerifier'
     }
   ],
   plugins: [
     babel({
-      babelrc: false,
-      presets: [['env', { modules: false }]],
-      plugins: ['external-helpers', 'transform-object-rest-spread']
+      exclude: 'node_modules/**',
+      presets: [['env', {
+        modules: false, 'targets': {
+          'node': 'current'
+        }
+      }]],
+      plugins: ['transform-object-rest-spread', 'external-helpers']
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify('production')
@@ -23,6 +28,7 @@ export default {
     resolve({
       browser: true,
       preferBuiltins: true
-    })
+    }) ,
+    terser()
   ]
 };
