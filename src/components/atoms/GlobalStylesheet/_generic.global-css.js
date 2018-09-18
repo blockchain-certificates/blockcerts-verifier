c70ceb9c3ce47f88e13de0a58e46ea0786b432bf
Julien Fraichot <jfraichot@learningmachine.com>
2018-09-18 14:17:29 +0200

    fix(Design): only maintain background when a certificate is loaded

--- src/components/atoms/GlobalStylesheet/_generic.global-css.js
+++ src/components/atoms/GlobalStylesheet/_generic.global-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-c-verifier-main{font-family:'Open Sans', -apple-system, BlinkMacSystemFont, sans-serif;color:#031532;position:relative;background-color:#fff}.buv-c-verifier-body--padded{padding:20px}
+export default html`<style>.buv-c-verifier-main{font-family:'Open Sans', -apple-system, BlinkMacSystemFont, sans-serif;color:#031532;position:relative}.buv-c-verifier-body--padded{background-color:#fff;padding:20px}
 </style>`;
