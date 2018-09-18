c8d7449e945d52dca509fbe76141ec8ae46c253d
Julien Fraichot <jfraichot@learningmachine.com>
2018-09-18 12:44:47 +0200

    fix(Design): add background color to whole component and change modal bg

--- src/components/atoms/GlobalStylesheet/_generic.global-css.js
+++ src/components/atoms/GlobalStylesheet/_generic.global-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-c-verifier-main{font-family:'Open Sans', -apple-system, BlinkMacSystemFont, sans-serif;color:#031532;position:relative}.buv-c-verifier-body{padding:20px}
+export default html`<style>.buv-c-verifier-main{font-family:'Open Sans', -apple-system, BlinkMacSystemFont, sans-serif;color:#031532;position:relative}.buv-c-verifier-body{padding:20px;background-color:rgba(3,21,50,0.7)}
 </style>`;
