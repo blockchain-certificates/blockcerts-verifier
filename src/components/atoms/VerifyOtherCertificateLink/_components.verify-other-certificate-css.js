04a6d42db1590f5bd5b35870863b8e9fdad8eec4
Julien Fraichot <jfraichot@learningmachine.com>
2018-10-04 14:42:46 +0200

    feat(CSS): update styles with shared-styles

--- src/components/atoms/VerifyOtherCertificateLink/_components.verify-other-certificate-css.js
+++ src/components/atoms/VerifyOtherCertificateLink/_components.verify-other-certificate-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-o-link{cursor:pointer;text-decoration:none;color:#031532}.buv-o-link--contrast{color:#fff}.buv-o-link__text--underline{border-bottom:1px solid}.buv-o-text-12{font-size:12px}.buv-o-text-15{font-size:15px;line-height:20px}.buv-c-verify-other-certificate{display:block}
+export default html`<style>.buv-o-link{cursor:pointer;text-decoration:none;color:#031532}.buv-o-link--contrast{color:#fff}.buv-o-link__text--underline{border-bottom:1px solid}.buv-o-text-11{font-size:11px}.buv-o-text-12{font-size:12px}.buv-o-text-15{font-size:15px;line-height:20px}.buv-c-verify-other-certificate{display:block}
 </style>`;
