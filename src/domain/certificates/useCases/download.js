6c8138e15af62f889e440bcccc8738e23a2fb3fe
Julien Fraichot <jfraichot@learningmachine.com>
2019-03-01 14:33:43 +0100

    feat(Download): [#32] enable download from any domain

--- src/domain/certificates/useCases/download.js
+++ src/domain/certificates/useCases/download.js
@@ -1,9 +1,5 @@
 import downloadFlag from '../../../constants/downloadFlag';
 
 export default function download (url) {
-  const lmRegex = /learningmachine\.(io|com)\/certificate/;
-
-  if (url.match(lmRegex)) {
-    return url + downloadFlag;
-  }
+  return url + downloadFlag;
 }
