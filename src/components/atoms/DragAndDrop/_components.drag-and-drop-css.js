2e032beed16709b22c4e8f6a6c9bd270c6c1815b
Julien Fraichot <jfraichot@learningmachine.com>
2019-07-26 15:44:58 +0200

    chore(Styles): regenerate styles

--- src/components/atoms/DragAndDrop/_components.drag-and-drop-css.js
+++ src/components/atoms/DragAndDrop/_components.drag-and-drop-css.js
@@ -1,3 +1,3 @@
-import { html } from '@polymer/lit-element';
+import {html} from '@polymer/lit-element';
 export default html`<style>.buv-o-text-11{font-size:11px}.buv-o-text-12{font-size:12px}.buv-o-text-15{font-size:15px;line-height:20px}.buv-c-drag-and-drop__droparea{z-index:100;background-color:rgba(0,0,0,0.1);display:none;position:absolute;top:0;left:0;width:100%;height:100%}.buv-c-drag-and-drop__droparea.is-active{display:block}
 </style>`;
