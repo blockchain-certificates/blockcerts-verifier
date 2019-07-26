7716a44bf7492fe2bc1c7500b01a3d2a2fd305ec
Julien Fraichot <jfraichot@learningmachine.com>
2019-07-26 16:01:31 +0200

    feat(Fullscreen): lay the HTML foundations of component

--- src/components/organisms/FullScreenCertificate/FullScreenCertificate.js
+++ src/components/organisms/FullScreenCertificate/FullScreenCertificate.js
@@ -1,9 +1,9 @@
 import { html } from '@polymer/lit-element';
-import { unsafeHTML } from 'lit-html/lib/unsafe-html.js';
 import CSS from './_components.fullscreen-certificate-css';
 import '../../atoms/CertificateDetails/index';
 import '../../atoms/FullCertificateV1';
 import '../../molecules/VerifyButton';
+import '../FullCertificate';
 
 export default function FullScreenCertificate ({
   hasCertificateDefinition,
@@ -19,10 +19,16 @@ export default function FullScreenCertificate ({
 
   return html`
     ${CSS}
-    <section class='buv-c-fullscreen-certificate'>${unsafeHTML(displayHTML)}</section>
-    <div class='buv-c-fullscreen-certificate__details'>
-      <buv-certificate-details class='buv-c-fullscreen-certificate__details-list'></buv-certificate-details>
-      <buv-verify-button class='buv-c-fullscreen-certificate__button' isHollow></buv-verify-button>
-    </div>
+    <section class='buv-c-fullscreen-certificate'>
+      <header><buv-verify-button class='buv-c-fullscreen-certificate__button'></buv-verify-button></header>
+      <section class="buv-c-fullscreen-certificate__content">
+        <div class='buv-c-fullscreen-certificate__details'>
+          <buv-certificate-details class='buv-c-fullscreen-certificate__details-list'></buv-certificate-details>
+        </div>
+        <div class='buv-c-fullscreen-certificate__certificate'>
+          <buv-full-certificate></buv-full-certificate>
+        </div>
+      </section>
+    </section>
   `;
 }
