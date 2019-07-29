9b7137a2bc7e774d77f4ced1d0c457c4261321c7
Julien Fraichot <jfraichot@learningmachine.com>
2019-07-29 14:40:52 +0200

    style(Styles): regenerate no space and turn off linting

--- src/components/organisms/VerificationModal/_components.verification-modal-css.js
+++ src/components/organisms/VerificationModal/_components.verification-modal-css.js
@@ -1,3 +1,3 @@
-import { html } from '@polymer/lit-element';
+import {html} from '@polymer/lit-element';
 export default html`<style>.buv-c-verification-modal__process{display:flex;justify-content:space-around}.buv-c-verification-modal__separator{margin-bottom:30px;background-color:rgba(3,21,50,0.05);height:1px;border:0 none}.buv-c-verification-modal__content{max-height:100vh;padding-bottom:43px;box-sizing:border-box;overflow:auto}@media only screen and (max-width: 600px){.buv-c-verification-modal__content{max-height:calc(100vh - 120px)}}
 </style>`;
