6ec06306dcac3f17bf43382153c611abba490c6d
Julien Fraichot <jfraichot@learningmachine.com>
2019-08-02 18:43:10 +0200

    test(FinalVerificationStep): add tests

--- test/application/components/atoms/FinalVerificationStep.spec.js
+++ test/application/components/atoms/FinalVerificationStep.spec.js
@@ -1,3 +1,64 @@
-describe('', function () {
+import FinalVerificationStep from '../../../../src/components/atoms/FinalVerificationStep/FinalVerificationStep';
+import { assertClassInStringBits, assertStringInValues } from '../helpers/assertClass';
 
+describe('FinalVerificationStep component test suite', function () {
+  describe('given the isVisible flag is set to true', function () {
+    it('should add the is-visible class', function () {
+      const instance = FinalVerificationStep({ isVisible: true });
+      expect(assertClassInStringBits(instance, 'is-visible')).toBe(true);
+    });
+  });
+
+  describe('given the isVisible flag is set to false', function () {
+    it('should not add the is-visible class', function () {
+      const instance = FinalVerificationStep({ isVisible: false });
+      expect(assertClassInStringBits(instance, 'is-visible')).toBe(false);
+    });
+  });
+
+  describe('given the isTestChain flag is set to true', function () {
+    it('should add the is-test class', function () {
+      const instance = FinalVerificationStep({ isTestChain: true });
+      expect(assertClassInStringBits(instance, 'is-test')).toBe(true);
+    });
+  });
+
+  describe('given the isTestChain flag is set to false', function () {
+    it('should not add the is-test class', function () {
+      const instance = FinalVerificationStep({ isTestChain: false });
+      expect(assertClassInStringBits(instance, 'is-test')).toBe(false);
+    });
+  });
+
+  describe('given the chain is provided', function () {
+    it('should replace the chain in the text', function () {
+      const fixtureFinalStep = {
+        // eslint-disable-next-line no-template-curly-in-string
+        description: 'here we test ${chain}'
+      };
+
+      const instance = FinalVerificationStep({ chain: 'Bitcoin', finalStep: fixtureFinalStep });
+      expect(assertStringInValues(instance, 'here we test Bitcoin')).toBe(true);
+    });
+  });
+
+  describe('given there is a link text', function () {
+    it('should use the transaction link', function () {
+      const fixtureFinalStep = {
+        linkText: 'Check transaction'
+      };
+
+      const instance = FinalVerificationStep({ transactionLink: 'http://test.com', finalStep: fixtureFinalStep });
+      expect(assertStringInValues(instance, 'http://test.com')).toBe(true);
+    });
+
+    it('should use the linkText', function () {
+      const fixtureFinalStep = {
+        linkText: 'Check transaction'
+      };
+
+      const instance = FinalVerificationStep({ transactionLink: 'http://test.com', finalStep: fixtureFinalStep });
+      expect(assertStringInValues(instance, 'Check transaction')).toBe(true);
+    });
+  });
 });
--- test/application/components/helpers/assertClass.js
+++ test/application/components/helpers/assertClass.js
@@ -1,6 +1,14 @@
 export function assertClassInStringBits (instance, className) {
   return instance.values.some(value => {
+    if (typeof value === 'string') {
+      return value.indexOf(className) > -1;
+    }
+
     if (value == null) {
+      return false;
+    }
+
+    if (value.strings == null) {
       return false;
     }
 
@@ -11,5 +19,19 @@ export function assertClassInStringBits (instance, className) {
 
       return string.indexOf(className) > -1;
     });
+  });
+}
+
+export function assertStringInValues (instance, string) {
+  return instance.values.some(value => {
+    if (value == null) {
+      return false;
+    }
+
+    if (value.values == null) {
+      return false;
+    }
+
+    return value.values.indexOf(string) > -1;
   });
 }
