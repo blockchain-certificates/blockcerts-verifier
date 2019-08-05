bb4ca1d363ba3892684aa55706a5e80a57a52e5a
Julien Fraichot <jfraichot@learningmachine.com>
2019-08-05 16:20:46 +0200

    feat(BackgroundVerification): reset verification status before verifying another certificate

--- test/application/actions/resetCertificateDefinition.spec.js
+++ test/application/actions/resetCertificateDefinition.spec.js
@@ -4,16 +4,23 @@ import resetCertificateDefinition from '../../../src/actions/resetCertificateDef
 import updateCertificateDefinition from '../../../src/actions/updateCertificateDefinition';
 import getInitialState from '../../../src/store/getInitialState';
 import validCertificateFixture from '../../fixtures/valid-certificate-example';
+import validCertificateStepsAssertions from '../../assertions/validCertificateSteps';
 import initialValidCertificateSteps from '../../assertions/initialValidCertificateSteps';
+import { getVerificationStatus } from '../../../src/selectors/verification';
+import VERIFICATION_STATUS from '../../../src/constants/verificationStatus';
+import updateVerificationStatus from '../../../src/actions/updateVerificationStatus';
 
 describe('resetCertificateDefinition action creator test suite', function () {
   let store;
 
   beforeEach(function () {
     const initialStateConfiguration = getInitialState({ disableAutoVerify: true });
+    // mock verified steps
+    initialStateConfiguration.verifiedSteps = validCertificateStepsAssertions;
     store = configureStore(initialStateConfiguration);
     // initially set a certificate definition in the state
     store.dispatch(updateCertificateDefinition(validCertificateFixture));
+    store.dispatch(updateVerificationStatus(VERIFICATION_STATUS.SUCCESS));
   });
 
   afterEach(function () {
@@ -32,5 +39,12 @@ describe('resetCertificateDefinition action creator test suite', function () {
     const state = store.getState();
 
     expect(getCertificateDefinition(state)).toBe(null);
+  });
+
+  it('should reset the verification status to DEFAULT', function () {
+    store.dispatch(resetCertificateDefinition());
+    const state = store.getState();
+
+    expect(getVerificationStatus(state)).toBe(VERIFICATION_STATUS.DEFAULT);
   });
 });
