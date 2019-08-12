fd259f6db3700c4f0875a43e8ef4c788b131d20c
Julien Fraichot <jfraichot@learningmachine.com>
2019-08-12 13:35:56 +0200

    feat(BackgroundVerification): show failure icon in final step

--- src/components/atoms/FileUpload/_components.file-upload-css.js
+++ src/components/atoms/FileUpload/_components.file-upload-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-o-link{cursor:pointer;text-decoration:none;color:inherit}.buv-o-link__text--underline{border-bottom:1px solid}.buv-o-text-11{font-size:11px}.buv-o-text-12{font-size:12px}.buv-o-text-15{font-size:15px;line-height:20px}.buv-u-visually-hidden{position:absolute !important;clip:rect(1px 1px 1px 1px);clip:rect(1px, 1px, 1px, 1px);padding:0 !important;border:0 !important;height:1px !important;width:1px !important;overflow:hidden}
+export default html`<style>.buv-o-link{cursor:pointer;text-decoration:none;color:currentColor}.buv-o-link__text--underline{border-bottom:1px solid}.buv-o-text-11{font-size:11px}.buv-o-text-12{font-size:12px}.buv-o-text-15{font-size:15px;line-height:20px}.buv-u-visually-hidden{position:absolute !important;clip:rect(1px 1px 1px 1px);clip:rect(1px, 1px, 1px, 1px);padding:0 !important;border:0 !important;height:1px !important;width:1px !important;overflow:hidden}
 </style>`;
--- src/components/atoms/VerifyOtherCertificateLink/_components.verify-other-certificate-css.js
+++ src/components/atoms/VerifyOtherCertificateLink/_components.verify-other-certificate-css.js
@@ -1,3 +1,3 @@
 import {html} from '@polymer/lit-element';
-export default html`<style>.buv-o-link{cursor:pointer;text-decoration:none;color:inherit}.buv-o-link__text--underline{border-bottom:1px solid}.buv-o-text-11{font-size:11px}.buv-o-text-12{font-size:12px}.buv-o-text-15{font-size:15px;line-height:20px}.buv-c-verify-other-certificate{display:block}
+export default html`<style>.buv-o-link{cursor:pointer;text-decoration:none;color:currentColor}.buv-o-link__text--underline{border-bottom:1px solid}.buv-o-text-11{font-size:11px}.buv-o-text-12{font-size:12px}.buv-o-text-15{font-size:15px;line-height:20px}.buv-c-verify-other-certificate{display:block}
 </style>`;
