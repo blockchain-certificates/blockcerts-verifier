6558f0934d2290a16342743815b865132eaaef73
dariomas <dariomas@users.noreply.github.com>
2019-09-16 16:53:59 +0200

    feat(i18n): add Italian

---
+++ src/i18n/lang/it.js
@@ -0,0 +1,50 @@
+export default {
+  errors: {
+    errorLabel: 'Errore',
+    invalidBlockcerts: 'Non è una definizione valida di Blockcerts.',
+    invalidBlockcertsUrl: 'Non è un URL di certificato valido.',
+    invalidFormatDragAndDrop: 'Sono accettati solo file JSON',
+    invalidUrl: 'Questo non sembra essere un URL valido.',
+    noDownloadLink: 'Link per il download non fornito!',
+    noMetadata: 'Nessun Metadata specificato per questo record',
+    noShareUrl: 'Non c\'è un URL da condividere!',
+    noTransactionId: 'Nessun ID transazione'
+  },
+  text: {
+    brandName: 'Blockcerts',
+    blockcertsHint: 'Visita il sito Web Blockcerts',
+    by: 'da',
+    certified: 'Certificato da',
+    closeButton: 'Fai clic per chiudere',
+    downloadLink: 'Scarica il Record in formato JSON',
+    dragAndDropHint: '(puoi anche trascinare il file).',
+    fileUpload: 'Scegli il file JSON',
+    issued: 'Rilasciato il',
+    issueDate: 'Data',
+    issuerName: 'Organizzazione Issuer',
+    issuerPublicKey: 'Chiave pubblica del Issuer',
+    item: 'Elemento',
+    itemPlural: 'Elementi',
+    metadataButton: 'Visualizza Metadata',
+    metadataTitle: 'Metadata del Certificato',
+    motto: 'Blockcerts, The Open Standard for Blockchain Credentials',
+    recipient: 'Titolare',
+    share: 'Condividi',
+    shareButton: 'Condividi sui Social Network',
+    signed: 'Firmato da',
+    substepsListClose: 'Nascondi',
+    substepsListHint: 'Mostra gli step secondari',
+    transactionId: 'ID transazione',
+    urlInput: 'Inserisci l\'URL del certificato',
+    urlInputPlaceholder: 'URL del certificato',
+    verificationStepProgress: 'Step di verifica...',
+    verify: 'Verifica',
+    verifyAgain: 'Verifica di nuovo',
+    verifyOther: 'Verifica un altro record',
+    viewRecord: 'Visualizza il Record'
+  },
+  date: {
+    months: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
+    pattern: 'DD MM YYYY'
+  }
+};
