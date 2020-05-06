5aa6ec33f7b410c0cf7e9960909098a5c5f95eba
Julien Fraichot <jfraichot@learningmachine.com>
2020-05-06 17:19:18 +0200

    chore(Dev): account for typescript in dev mode

---
+++ rollup.dev.config.js
@@ -0,0 +1,34 @@
+import serve from 'rollup-plugin-serve';
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
+import livereload from 'rollup-plugin-livereload';
+
+const BUILD_OUTPUT_FOLDER = 'dev';
+
+export default [
+  {
+    input: 'src/blockcerts-verifier/index.js',
+    output: [
+      {
+        file: `${BUILD_OUTPUT_FOLDER}/index.js`,
+        format: 'iife',
+        name: 'BlockcertsVerifier'
+      }
+    ],
+    plugins: [
+      typescript(),
+      resolve({
+        browser: true,
+        preferBuiltins: true
+      }),
+      serve({
+        contentBase: [BUILD_OUTPUT_FOLDER, 'demo', 'node_modules', 'dist'],
+        port: 8081,
+        open: true
+      }),
+      livereload({
+        watch: BUILD_OUTPUT_FOLDER
+      })
+    ]
+  }
+];
