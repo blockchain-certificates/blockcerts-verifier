415cffba00bdac89f9cfd150de3435d9cd304441
Julien Fraichot <jfraichot@learningmachine.com>
2020-05-13 15:40:16 +0200

    test(ExplorerAPIs): [#433] do not run e2e ts with jest

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,9 @@ module.exports = {
     '^.+\\.(ts)$': 'ts-jest',
     '^.+\\.(js)$': 'babel-jest'
   },
+  testPathIgnorePatterns: [
+    './test/e2e'
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!(@polymer|lit-html|pwa-helpers|@blockcerts/cert-verifier-js)/)'
   ]
