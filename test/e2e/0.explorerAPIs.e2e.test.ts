2032d206471e54a04f1e9db808f3037c2d9deb08
Julien Fraichot <jfraichot@learningmachine.com>
2021-02-08 21:09:26 +0100

    feat(Cvjs): use latest version
    We also reorder the karma test because they do not run in proper isolation and end up leaking on one another

--- test/e2e/0.explorerAPIs.e2e.test.ts
+++ test/e2e/0.explorerAPIs.e2e.test.ts
@@ -5,29 +5,19 @@ import { ExplorerAPI } from '@blockcerts/cert-verifier-js';
 import { wait } from './helpers/waitForKarma';
 
 describe('BlockcertsVerifier explorer apis e2e test suite', function () {
-  let element;
-  let parsingFunctionStub: sinon.SinonStub;
-
-  beforeEach(async function () {
-    parsingFunctionStub = sinon.stub().returns('');
-    element = await fixture(html`
-      <blockcerts-verifier></blockcerts-verifier>
-    `);
-  });
-
-  afterEach(function () {
-    parsingFunctionStub.resetHistory();
-  });
-
   describe('given the verification process is executed', function () {
     it('should call the custom explorer', async function () {
-      this.timeout(10000);
+      this.timeout(3000);
+      const parsingFunctionStub = sinon.stub().returns('{}');
       const explorerAPI: ExplorerAPI = {
         parsingFunction: parsingFunctionStub,
         priority: 0,
         // we provide a URL that returns a JSON to avoid any issue in cert verifier js explorer execution
         serviceURL: 'https://raw.githubusercontent.com/blockchain-certificates/blockcerts-verifier/master/test/fixtures/mainnet-valid-2.0.json'
       };
+      const element: any = await fixture(html`
+        <blockcerts-verifier></blockcerts-verifier>
+      `); // TODO: define type as BlockcertsVerifier
       element.explorerAPIs = [explorerAPI];
       await wait(10);
       element.src = 'https://blockcerts.learningmachine.com/certificate/fe53b323df845641b74e8d3f6d175ea0';
