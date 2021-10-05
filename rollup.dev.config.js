4782ab6cc42eb185e6000af2ea476570a5d8e628
Julien Fraichot <julien.fraichot@hyland.com>
2021-10-05 14:22:58 +0200

    chore(Dev): make dev mode https to allow crypto.subtle run

--- rollup.dev.config.js
+++ rollup.dev.config.js
@@ -2,6 +2,7 @@ import serve from 'rollup-plugin-serve';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import livereload from 'rollup-plugin-livereload';
+import fs from 'fs';
 
 const BUILD_OUTPUT_FOLDER = 'dev';
 
@@ -25,7 +26,11 @@ export default [
         contentBase: [BUILD_OUTPUT_FOLDER, 'demo', 'node_modules', 'dist', 'test/fixtures'],
         host: '0.0.0.0',
         port: 8081,
-        open: true
+        open: true,
+        https: {
+          cert: fs.readFileSync(`${BUILD_OUTPUT_FOLDER}/https-cert/cert.pem`),
+          key: fs.readFileSync(`${BUILD_OUTPUT_FOLDER}/https-cert/key.pem`)
+        }
       }),
       livereload({
         watch: BUILD_OUTPUT_FOLDER
