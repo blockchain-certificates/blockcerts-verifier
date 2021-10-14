a550691194ada6a6c1db8984350551d529bc1b99
Julien Fraichot <julien.fraichot@hyland.com>
2021-10-14 17:36:12 +0200

    fix(Certificate): maintain support of v1 certificate verification

--- src/domain/certificates/useCases/parse.ts
+++ src/domain/certificates/useCases/parse.ts
@@ -1,17 +1,29 @@
-import { Certificate, CertificateOptions } from '@blockcerts/cert-verifier-js';
+import { Certificate, CertificateOptions, retrieveBlockcertsVersion } from '@blockcerts/cert-verifier-js';
+import { Certificate as CertificateV1 } from '@blockcerts/cert-verifier-js-v1-legacy';
 
 export interface ICertificateObject {
   certificateDefinition: Certificate | null;
   errorMessage?: string;
 }
 
-export default async function parse (definition, options: CertificateOptions = {}): Promise<ICertificateObject> {
+function getVersion (definition: string): number {
+  const parsedDefinition = JSON.parse(definition);
+  return retrieveBlockcertsVersion(parsedDefinition['@context']);
+}
+
+export default async function parse (definition: string, options: CertificateOptions = {}): Promise<ICertificateObject> {
   if (!options.locale) {
     options.locale = 'auto';
   }
 
   try {
-    const certificateDefinition = new Certificate(definition, options);
+    const version: number = getVersion(definition);
+    let certificateDefinition;
+    if (version === 1) {
+      certificateDefinition = new CertificateV1(definition, options);
+    } else {
+      certificateDefinition = new Certificate(definition, options);
+    }
     await certificateDefinition.init();
 
     return {
