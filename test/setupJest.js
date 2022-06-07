db722ccf415790339d5128f596b71df59bfda964
Julien Fraichot <julien.fraichot@hyland.com>
2022-06-07 11:42:03 -0500

    chore(Test): fix bigInt to number error in jest transpilation

--- test/setupJest.js
+++ test/setupJest.js
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 require('jest-fetch-mock');
 
 global.MutationObserver = class {
@@ -8,3 +9,11 @@ global.MutationObserver = class {
 global.customElements = {
   define: () => {}
 };
+
+Object.defineProperty(global.self, 'crypto', {
+  value: {
+    // use node 15.x
+    subtle: crypto.webcrypto.subtle,
+    getRandomValues: () => new Uint32Array(10)
+  }
+});
