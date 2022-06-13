e1522fc8172f8b57070c02eed221525162395989
Julien Fraichot <julien.fraichot@hyland.com>
2022-06-13 12:18:51 -0500

    test(Date): fix developer timezone issue when running test

--- test/application/i18n/getDateFormat.spec.ts
+++ test/application/i18n/getDateFormat.spec.ts
@@ -1,8 +1,38 @@
 import currentLocale from '../../../src/i18n/valueObjects/currentLocale';
 import getDateFormat from '../../../src/i18n/getDateFormat';
 
+class MockDate {
+  private readonly date: string;
+
+  constructor (date) {
+    this.date = date;
+  }
+
+  getMonth (): number {
+    return parseInt(this.date.split('-')[1], 10) - 1;
+  }
+
+  getDate (): number {
+    return parseInt(this.date.split('-')[2], 10);
+  }
+
+  getFullYear (): number {
+    return parseInt(this.date.split('-')[0], 10);
+  }
+}
+
 describe('getDateFormat test suite', function () {
   describe('given a date', function () {
+    let initialDate;
+    beforeEach(function () {
+      initialDate = global.Date;
+      global.Date = MockDate as any;
+    });
+
+    afterEach(function () {
+      global.Date = initialDate;
+    });
+
     describe('and the current language is English', function () {
       it('should return the correct format', function () {
         currentLocale.locale = 'en';
