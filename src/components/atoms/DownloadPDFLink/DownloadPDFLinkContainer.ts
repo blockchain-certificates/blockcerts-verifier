5d5fecebf850da6f7ef0eea618e10e91b03fbb8f
Matthieu COLLÉ <druidev+github@gmail.com>
2022-06-16 15:50:16 -0400

    feat(DownloadPdf): added container

--- src/components/atoms/DownloadPDFLink/DownloadPDFLinkContainer.ts
+++ src/components/atoms/DownloadPDFLink/DownloadPDFLinkContainer.ts
@@ -1,41 +1,39 @@
 import connector from '../../../store/connector';
-import resetCertificateDefinition from '../../../actions/resetCertificateDefinition';
 import DownloadPDFLink from './DownloadPDFLink';
 import {
   getCertificateDefinition,
   getCertificateTitle,
-  getDisplayAsHTML,
   getDisplayContent,
   getDisplayContentEncoding,
   getDisplayContentType,
   getIssueDate,
-  getIssuedOn, getIssuerLogo,
+  getIssuerLogo,
   getIssuerName,
-  getRecipientName, getRecordLink
+  getIssuerPublicKey,
+  getRecipientName,
+  getRecordLink
 } from '../../../selectors/certificate';
-
-export const mapDispatchToProps = {
-  onClick: resetCertificateDefinition
-};
 
 export const mapStateToProps = (state) => ({
   isVisible: !!getCertificateDefinition(state),
   contentType: getDisplayContentType(state),
   contentEncoding: getDisplayContentEncoding(state),
   content: getDisplayContent(state),
-  displayHTML: getDisplayAsHTML(state),
   recipientName: getRecipientName(state),
   certificateTitle: getCertificateTitle(state),
   issueDate: getIssueDate(state),
-  issuedOn: getIssuedOn(state),
   issuerName: getIssuerName(state),
   issuerLogo: getIssuerLogo(state),
-  recordLink: getRecordLink(state)
+  recordLink: getRecordLink(state),
+  issuerPublicKey: getIssuerPublicKey(state)
 });
 
 const ownProps = {
   display: String
 };
 
-const DownloadPDFLinkContainer = connector(DownloadPDFLink, { mapDispatchToProps, mapStateToProps, ownProps });
+const DownloadPDFLinkContainer = connector(
+  DownloadPDFLink,
+  { mapStateToProps, ownProps }
+);
 export default DownloadPDFLinkContainer;
