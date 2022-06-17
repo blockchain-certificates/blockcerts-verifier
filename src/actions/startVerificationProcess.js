baaf347fe1f0051eab2fda00249701e4130c7544
Matthieu COLLÉ <druidev+github@gmail.com>
2022-06-17 15:44:54 -0400

    refactor(ActionTypes): fixed typo in startVerificationProcess

--- src/actions/startVerificationProcess.js
+++ src/actions/startVerificationProcess.js
@@ -5,7 +5,7 @@ import verifyCertificate from './verifyCertificate';
 export default function startVerificationProcess () {
   return function (dispatch) {
     dispatch({
-      type: ACTIONS.START_VERFICATION_PROCESS
+      type: ACTIONS.START_VERIFICATION_PROCESS
     });
 
     dispatch(showVerificationModal(true));
