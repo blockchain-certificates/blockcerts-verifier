4b65a06df5b841797a9af0a4e130d83914e88d01
Matthieu COLLÉ <druidev+github@gmail.com>
2022-06-17 19:51:07 +0200

    feat(DownloadPdf): conditionally render link

--- src/components/organisms/CardCertificate/CardCertificate.js
+++ src/components/organisms/CardCertificate/CardCertificate.js
@@ -27,7 +27,8 @@ export default function CardCertificate (props) {
     issueDate,
     issuerName,
     recordLink,
-    hideVerifyButton
+    hideVerifyButton,
+    disableDownloadPdf
   } = props;
 
   let { hideRecordLink } = props;
@@ -55,9 +56,11 @@ export default function CardCertificate (props) {
           <h1 class$=${titleClass}>${certificateTitle}</h1>
           <h2 class$='${titleClass}  buv-c-card__recipient'>${recipientName}</h2>
           <span class='buv-o-text-12'>${getText('text.issued')} <time datetime$='${issuedOn}'>${issueDate}</time> ${getText('text.by')} ${issuerName}</span>
-          <div class='buv-c-card__download-button'>
+          ${disableDownloadPdf
+            ? ''
+            : html`<div class='buv-c-card__download-button'>
             <buv-download-pdf-link></buv-download-pdf-link>
-          </div>
+          </div>`}
         </div>
       ${
   hideRecordLink
