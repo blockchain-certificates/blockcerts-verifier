d6fe4375af57bffaf5ba782231b581d8ceb52251
Matthieu COLLÉ <druidev+github@gmail.com>
2022-06-17 19:28:12 +0200

    test(DownloadPdf): updated getPDFCoverPage tests

--- test/application/helpers/getPDFCoverPage.spec.ts
+++ test/application/helpers/getPDFCoverPage.spec.ts
@@ -3,6 +3,9 @@ import COVER_PAGE_ALL_ELEMENTS from './fixtures/fixture-cover-page-all-elements'
 import COVER_PAGE_NO_CERTIFICATE_TITLE from './fixtures/fixture-cover-page-no-certificate-title';
 import COVER_PAGE_NO_ISSUER_LOGO from './fixtures/fixture-cover-page-no-issuer-logo';
 import COVER_PAGE_NO_QR_CODE from './fixtures/fixture-cover-page-no-qr-code';
+import * as BlockcertsLogoComponent from '../../../src/components/atoms/BlockcertsLogo/BlockcertsLogo';
+import sinon from 'sinon';
+import { html } from 'lit-html';
 
 const mockCertificateTitle = 'mock certificate title';
 const mockIssueDate = '00-00-0000 00:00:00';
@@ -13,6 +16,17 @@ const mockQRCodeImage = 'Mock QR Code Image';
 const mockIssuerPublicKey = 'Mock Issuer Public Key';
 
 describe('getPDFCoverPage function', function () {
+  let stubLogoFunction: sinon.SinonStub;
+
+  beforeEach(function () {
+    // Stubbing the logo helper since it doesn't render variables
+    stubLogoFunction = sinon.stub(BlockcertsLogoComponent, 'logoWithBranding').returns(html`<img src>`);
+  });
+
+  afterEach(function () {
+    stubLogoFunction.restore();
+  });
+
   describe('given all the Blockcerts info is passed', function () {
     it('should return the correct cover page template', function () {
       const result: HTMLElement = getPDFCoverPage({
