1092c8852deb53180d2fd96ee0e7cb86b048c9bf
Matthieu COLLÉ <druidev+github@gmail.com>
2022-07-05 10:59:57 -0400

    Revert "chore(Ci): updated auomation paths"
    This reverts commit a73809073848bc20ec4ee1d00af85a7d4fafcf66.

--- scripts/automated-update-blockcerts.org.sh
+++ scripts/automated-update-blockcerts.org.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-source ./scripts/automated-update-blockcerts-verifier-in-other-repo.sh
+source ./automated-update-blockcerts-verifier-in-other-repo.sh
 
 automated_update_bv_in_repo "blockchain-certificates-web" "blockchain-certificates-web.github.io" "feat/update-bv" "botcerts"
