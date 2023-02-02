bbf575b735924bb61777fd4fa8eb584169244dc5
Julien Fraichot <julien.fraichot@hyland.com>
2023-02-02 17:17:59 +0100

    refactor(FullCertificate): comvert to class

--- src/components/organisms/FullCertificate/FullCertificate.ts
+++ src/components/organisms/FullCertificate/FullCertificate.ts
@@ -1,4 +1,4 @@
-import { html } from '@polymer/lit-element';
+import { html, LitElement } from '@polymer/lit-element';
 import { TemplateResult } from 'lit-html';
 import { unsafeHTML } from 'lit-html/lib/unsafe-html.js';
 import CSS from './_components.full-certificate-css';
@@ -25,20 +25,29 @@ function renderDisplayHTML (displayHTML: string, clickableUrls: boolean): Templa
 
 export interface IFullCertificate {
   clickableUrls?: boolean;
-  hasCertificateDefinition: boolean;
+  hasCertificateDefinition?: boolean;
   displayHTML?: string;
 }
 
-export default function FullCertificate ({
-  clickableUrls,
-  hasCertificateDefinition,
-  displayHTML
-}: IFullCertificate): TemplateResult {
-  if (!hasCertificateDefinition) {
-    return null;
+export class FullCertificateComponent extends LitElement {
+  static get properties () {
+    return {
+      clickableUrls: Boolean,
+      hasCertificateDefinition: Boolean,
+      displayHTML: String
+    };
   }
 
-  return html`
+  _render ({
+    clickableUrls,
+    hasCertificateDefinition,
+    displayHTML
+  }: IFullCertificate): TemplateResult {
+    if (!hasCertificateDefinition) {
+      return null;
+    }
+
+    return html`
     ${CSS}
     ${displayHTML ? renderDisplayHTML(displayHTML, clickableUrls) : html`<buv-full-certificate-v1></buv-full-certificate-v1>`}
     <div class='buv-c-full-certificate__details'>
@@ -48,4 +57,19 @@ export default function FullCertificate ({
       </buv-final-verification-step>
     </div>
   `;
+  }
 }
+
+window.customElements.define('buv-full-certificate-raw', FullCertificateComponent);
+function FullCertificateWrapper (props: IFullCertificate) {
+  return html`
+    <buv-full-certificate-raw
+      clickableUrls = '${props.clickableUrls}'
+      hasCertificateDefinition = '${props.hasCertificateDefinition}'
+      displayHTML = '${props.displayHTML}'
+    ></buv-full-certificate-raw>`;
+}
+
+export {
+  FullCertificateWrapper as FullCertificate
+};
--- test/application/components/organisms/FullCertificate.spec.ts
+++ test/application/components/organisms/FullCertificate.spec.ts
@@ -1,72 +1,100 @@
-import * as litUnsafeHTML from 'lit-html/lib/unsafe-html.js';
 import sinon from 'sinon';
-import FullCertificate from '../../../../src/components/organisms/FullCertificate/FullCertificate';
-import { assertStringInValues } from '../helpers/assertStringValues';
+import * as litUnsafeHTML from 'lit-html/lib/unsafe-html.js';
+import { FullCertificateComponent, FullCertificate } from '../../../../src/components/organisms/FullCertificate/FullCertificate';
+import { assertClassInStringBits, assertStringInValues } from '../helpers/assertStringValues';
 
-describe('FullCertificate component test suite', function () {
-  describe('given there is no certificate definition', function () {
-    it('should return null', function () {
-      const instance = FullCertificate({ hasCertificateDefinition: false });
-      expect(instance).toBeNull();
+describe('FullCertificate wrapper function test suite', function () {
+  describe('setting the attributes of the API to the webcomponent', function () {
+    it('- clickableUrls', function () {
+      const instance = FullCertificate({});
+      expect(instance.strings[0].includes('clickableUrls')).toBe(true);
+    });
+
+    it('- hasCertificateDefinition', function () {
+      const instance = FullCertificate({});
+      expect(instance.strings[1].includes('hasCertificateDefinition')).toBe(true);
+    });
+
+    it('- displayHTML', function () {
+      const instance = FullCertificate({});
+      expect(instance.strings[2].includes('displayHTML')).toBe(true);
     });
   });
 
-  describe('given the certificate has a displayHTML property', function () {
-    beforeEach(function () {
-      // call through unsafeHTML directive
-      sinon.stub(litUnsafeHTML, 'unsafeHTML').callsFake(str => str);
+  describe('passing the API values to the webcomponent', function () {
+    it('- clickableUrls', function () {
+      const instance = FullCertificate({ clickableUrls: true });
+      expect(instance.values[0]).toBe(true);
     });
 
-    afterEach(function () {
-      sinon.restore();
-    });
-
-    it('should render the displayHTML property', function () {
-      const fixtureDisplayHTML = '<div>This is a test</div>';
-      const instance = FullCertificate({ displayHTML: fixtureDisplayHTML, hasCertificateDefinition: true });
-      expect(assertStringInValues(instance, fixtureDisplayHTML)).toBe(true);
-    });
-
-    describe('given the displayHtml is legacy', function () {
-      it('should render the displayHtml inside a parent with a width constrain CSS class', function () {
-        const fixtureDisplayHTML = '<section class="text">This is a test</section>';
-        const instance = FullCertificate({ displayHTML: fixtureDisplayHTML, hasCertificateDefinition: true });
-        expect(assertStringInValues(instance, 'buv-c-certificate--fixed-width')).toBe(true);
-      });
-    });
-
-    describe('given the displayHtml is not legacy', function () {
-      it('should render the displayHtml inside a parent without a width constrain CSS class', function () {
-        const fixtureDisplayHTML = '<div>This is a test</div>';
-        const instance = FullCertificate({ displayHTML: fixtureDisplayHTML, hasCertificateDefinition: true });
-        expect(assertStringInValues(instance, 'buv-c-certificate--fixed-width')).toBe(false);
-      });
-    });
-
-    describe('and the displayHTML property contains a URL', function () {
-      describe('and the clickableUrls flag is set to true', function () {
-        it('should transform it to a clickable link', function () {
-          const fixtureDisplayHTML = '<div>This is a test, click on www.blockcerts.org</div>';
-          const instance = FullCertificate({ displayHTML: fixtureDisplayHTML, hasCertificateDefinition: true, clickableUrls: true });
-          expect(assertStringInValues(instance, '<a href="http://www.blockcerts.org" target="_blank" rel="noopener noreferrer">www.blockcerts.org</a>')).toBe(true);
-        });
-      });
-
-      describe('and the clickableUrls flag is set to false', function () {
-        it('should not transform it to a clickable link', function () {
-          const fixtureDisplayHTML = '<div>This is a test, click on www.blockcerts.org</div>';
-          const instance = FullCertificate({ displayHTML: fixtureDisplayHTML, hasCertificateDefinition: true, clickableUrls: false });
-          expect(assertStringInValues(instance, '<a href="http://www.blockcerts.org" target="_blank" rel="noopener noreferrer">www.blockcerts.org</a>')).toBe(false);
-        });
-      });
-    });
-  });
-
-  describe('given the certificate does not have a displayHTML property', function () {
-    it('should render the certificate as v1', function () {
+    it('- hasCertificateDefinition', function () {
       const instance = FullCertificate({ hasCertificateDefinition: true });
-      const instanceAsString = JSON.stringify(instance);
-      expect(instanceAsString).toContain('buv-full-certificate-v1');
+      expect(instance.values[1]).toBe(true);
+    });
+
+    it('- displayHTML', function () {
+      const fixtureHtml = '<div>Yo</div>';
+      const instance = FullCertificate({ displayHTML: fixtureHtml });
+      expect(instance.values[2]).toBe(fixtureHtml);
+    });
+  });
+});
+describe('FullCertificate component test suite', function () {
+  describe('_render method', function () {
+    describe('given there is no certificate definition', function () {
+      it('should return null', function () {
+        const instance = new FullCertificateComponent();
+        const result = instance._render({ hasCertificateDefinition: false });
+        expect(result).toBeNull();
+      });
+    });
+
+    describe('given the certificate has a displayHTML property', function () {
+      let instance;
+
+      beforeEach(function () {
+        instance = new FullCertificateComponent();
+        // call through unsafeHTML directive
+        sinon.stub(litUnsafeHTML, 'unsafeHTML').callsFake(str => str);
+      });
+
+      afterEach(function () {
+        instance = null;
+        sinon.restore();
+      });
+
+      it('should render the displayHTML property', function () {
+        const fixtureDisplayHTML = '<div>This is a test</div>';
+        const result = instance._render({ displayHTML: fixtureDisplayHTML, hasCertificateDefinition: true });
+        expect(assertStringInValues(result, fixtureDisplayHTML)).toBe(true);
+      });
+
+      describe('and the displayHTML property contains a URL', function () {
+        describe('and the clickableUrls flag is set to true', function () {
+          it('should transform it to a clickable link', function () {
+            const fixtureDisplayHTML = '<div>This is a test, click on www.blockcerts.org</div>';
+            const result = instance._render({ displayHTML: fixtureDisplayHTML, hasCertificateDefinition: true, clickableUrls: true });
+            expect(assertStringInValues(result, '<a href="http://www.blockcerts.org" target="_blank" rel="noopener noreferrer">www.blockcerts.org</a>')).toBe(true);
+          });
+        });
+
+        describe('and the clickableUrls flag is set to false', function () {
+          it('should not transform it to a clickable link', function () {
+            const fixtureDisplayHTML = '<div>This is a test, click on www.blockcerts.org</div>';
+            const result = instance._render({ displayHTML: fixtureDisplayHTML, hasCertificateDefinition: true, clickableUrls: false });
+            expect(assertStringInValues(result, '<a href="http://www.blockcerts.org" target="_blank" rel="noopener noreferrer">www.blockcerts.org</a>')).toBe(false);
+          });
+        });
+      });
+    });
+
+    describe('given the certificate does not have a displayHTML property', function () {
+      it('should render the certificate as v1', function () {
+        const instance = new FullCertificateComponent();
+        const result = instance._render({ hasCertificateDefinition: true });
+        const instanceAsString = JSON.stringify(result);
+        expect(instanceAsString).toContain('buv-full-certificate-v1');
+      });
     });
   });
 });
