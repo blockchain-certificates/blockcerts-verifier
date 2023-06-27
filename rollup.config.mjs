b2cc69a398c4a6ea52cf67eee4fa8bb002bb2063
Julien Fraichot <julien.fraichot@hyland.com>
2023-06-27 15:48:45 +0200

    chore(Bundle): bump rollup dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import replace from 'rollup-plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 export default [
