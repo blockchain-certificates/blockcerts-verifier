c20c16a793077ce65dcc4807d84772df76e1464b
Julien Fraichot <julien.fraichot@hyland.com>
2023-11-03 13:59:48 +0100

    test(Jest): fix jest tests

--- src/domain/certificates/useCases/parse.ts
+++ src/domain/certificates/useCases/parse.ts
@@ -1,14 +1,16 @@
 import {
-  Blockcerts,
-  BlockcertsVersion,
   Certificate,
-  CertificateOptions,
   retrieveBlockcertsVersion
 } from '@blockcerts/cert-verifier-js';
 import {
   Certificate as CertificateV1,
   CertificateOptions as CertificateOptionsV1
 } from '@blockcerts/cert-verifier-js-v1-legacy';
+import type {
+  Blockcerts,
+  BlockcertsVersion,
+  CertificateOptions
+} from '@blockcerts/cert-verifier-js';
 
 export interface ICertificateObject {
   certificateDefinition: Certificate | null;
