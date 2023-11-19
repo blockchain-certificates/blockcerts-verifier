d7e572dc37773adefa177bb977b63d2d0857f9a7
amazanzan <54779702+amazanzan@users.noreply.github.com>
2023-11-19 13:11:41 -0500

    Create ja.ts

---
+++ src/i18n/lang/ja.ts
@@ -0,0 +1,60 @@
+export default {
+  errors: {
+    errorLabel: 'エラー',
+    invalidBlockcerts: 'この証明書は有効なブロックサーツ証明書ではありません。発行者、もしくはこの証明書を提供した取得者にお問合せください。',
+    invalidBlockcertsUrl: '有効な証明書URLではありません。',
+    invalidFormatDragAndDrop: 'JSONファイルしか受理できません',
+    invalidUrl: 'このURLは無効のようです。',
+    noIssuerName: '名前が提供されていません',
+    noMetadata: 'この証明書に該当するメタデータがありません',
+    noShareUrl: 'シェアできるURLがありません！',
+    noTransactionId: '取引IDがありません'
+  },
+  text: {
+    brandName: 'Blockcerts',
+    blockcertsHint: 'Blockcertsのホームページへ移動',
+    by: '　発行者：',
+    certified: '証明者：',
+    closeButton: 'クリックで閉じる',
+    downloadLink: 'JSON形式で証明書をダウンロード',
+    downloadPDFLink: 'PDF形式で証明書をダウンロード',
+    downloadPDFLinkIsLoading: 'PDFを生成中...',
+    dragAndDropHint: '（ファイルのドラッグ&ドロップも可）。',
+    fileUpload: 'JSONファイルの選択',
+    issued: '発行時：',
+    issueDate: '発行日：',
+    issuerName: '発行者',
+    issuerPublicKey: '発行者の公開鍵',
+    issuerProfileDomain: '発行者情報',
+    item: '項目',
+    itemPlural: '項目',
+    metadataButton: 'メタデータの閲覧',
+    metadataTitle: 'メタデータ',
+    motto: 'Blockcerts：ブロックチェーン証明書のオープンスタンダード',
+    recipient: '取得者',
+    share: 'シェア先：',
+    shareButton: 'SNSでシェア',
+    signatureSuiteType: 'デジタル署名の書類',
+    signatureStatusSuccess: 'この署名の認証に成功しました',
+    signatureStatusTestSuccess: 'この署名の認証に成功しましたが、発行は試用のブロックチェーンで行われました。',
+    signatureStatusFailure: 'この署名の認証中にエラーが発生しました',
+    signed: '署名者：',
+    substepsListClose: '閉じる',
+    substepsListHint: 'サブステップのリストを開く',
+    transactionId: '取引ID',
+    urlInput: '証明書URLの入力',
+    urlInputPlaceholder: '証明書URL',
+    verificationStepProgress: '認証ステップを実行中...',
+    verify: '認証する',
+    verifyAgain: 'もう一度認証する',
+    verifyOther: '別の証明書を認証する',
+    viewRecord: '証明書を閲覧する'
+  },
+  date: {
+    months: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    pattern: 'YYYY-MM-DD'
+  },
+  numbers: {
+    ordinal: ['番目', '番目', '番目', '番目']
+  }
+};
