6f680af450f8d5b1de86ae14066e76cc353adc42
Julien Fraichot <julien.fraichot@hyland.com>
2024-11-20 15:36:36 +0100

    refactor(Selector): create certificate directory to define sub selectors

--- src/selectors/certificate/index.ts
+++ src/selectors/certificate/index.ts
@@ -1,13 +1,13 @@
-import domain from '../domain';
-import sanitize from '../../sanitizer/sanitizer';
-import getDateFormat from '../i18n/getDateFormat';
-import { isValidUrl } from '../helpers/validations';
-import { VERIFICATION_STATUSES } from '../constants/verificationStatuses';
-import { CONTENT_TYPES } from '../constants/contentTypes';
-import getText from '../i18n/getText';
+import domain from '../../domain';
+import sanitize from '../../../sanitizer/sanitizer';
+import getDateFormat from '../../i18n/getDateFormat';
+import { isValidUrl } from '../../helpers/validations';
+import { VERIFICATION_STATUSES } from '../../constants/verificationStatuses';
+import { CONTENT_TYPES } from '../../constants/contentTypes';
+import getText from '../../i18n/getText';
 import type { IVerificationMapItem, Signers, Certificate } from '@blockcerts/cert-verifier-js';
-import type { BlockcertsVerifierState, IFinalStep } from '../store/getInitialState';
-import type { V1Signature } from '../models/V1Signature';
+import type { BlockcertsVerifierState, IFinalStep } from '../../store/getInitialState';
+import type { V1Signature } from '../../models/V1Signature';
 
 export function getCertificateDefinition (state: BlockcertsVerifierState): Certificate {
   return state.certificateDefinition;
